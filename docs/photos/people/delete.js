77cfa903a4698bb8135b32dc8306f81e1ac164bf
Abdelrhman-Hosny <ahosny2792@gmail.com>
2021-04-15 07:42:06 +0200

    Added photos.people.delete (no err or example)

---
+++ docs/photos/people/delete.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @api {DELETE} /photo/people/delete delete
+ * @apiGroup photos.people
+ * @apiVersion  1.0.0
+ * 
+ * @apiDescription Removes a person from a photo
+ * 
+ * @apiParam  {String} api_key Your API application key.
+ * @apiParam  {String} photo_id The id of the photo to remove a person from.
+ * @apiParam  {String} user_id The id of the person to remove from the photo.
+ * 
+ * 
+ * 
+ * 
+ */
