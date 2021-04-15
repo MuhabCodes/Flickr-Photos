eea8b213af47d82975c33658bf1fcaa1c2ac4fcc
Abdelrhman-Hosny <ahosny2792@gmail.com>
2021-04-15 16:26:05 +0200

    Added groups/pools

---
+++ docs/groups/pools/add.js
@@ -0,0 +1,39 @@
+/**
+ * 
+ * @api {POST} /groups/:group_id/photos/:photo_id addPhoto
+ * @apiGroup groups.pools
+ * @apiVersion  1.0.0
+ * 
+ * @apiDescription Add a photo to a group's pool.
+ * 
+ * @apiParam  {String} api_key Your API application key
+ * @apiParam  {String} group_id The id of the group to add the photo to.
+ * @apiParam  {String} photo_id The id of the photo to add. Photo must belong to the calling user.
+ * 
+ * @apiSuccess (Success 200) {Number} statusCode The status code
+ * 
+ * 
+ * @apiError (Error 400) {String} error The server could not understand the request due to invalid syntax.
+ *@apiError (Error 400) {Number} statusCode The status code
+ *
+ * @apiError (Error 401) {String} error The user doesn't have permission to do this action
+ * @apiError (Error 401) {Number} statusCode The status code
+ *
+ *
+ * @apiError (Error 404) {String} error Informs the caller of the missing object.
+ * @apiError (Error 404) {Number} statusCode The status code
+ *
+ * @apiErrorExample {json} Error-404:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "The photo was not found.",
+ *        "status_code":404
+ *     }
+ *
+ * @apiErrorExample {json} Error-401:
+ *     HTTP/1.1 401 Unauthorized
+ *     {
+ *       "error": "Unauthorized User",
+ *        "status_code":401
+ *     }
+ */
---
+++ docs/groups/pools/getContext.js
@@ -0,0 +1,40 @@
+/**
+ * 
+ * @api {GET} /groups/:group_id/pools/photo/:photo_id/context getContext
+ * @apiGroup groups.pools
+ * @apiVersion  1.0.0
+ * 
+ * @apiDescription Get the two photos before and after a given photo.
+ * 
+ * @apiSuccess (Success 200) {Object[]} photoList A list containing the context pictures.
+ * @apiSuccess (Success 200) {Number} statusCode The status code
+ * 
+ * @apiParam  {String} api_key Your API application key.
+ * @apiParam  {String} photo_id The id of the photo.
+ * @apiParam  {String} group_id The id of the group where the photo exists.
+ * 
+ * @apiError (Error 400) {String} error The server could not understand the request due to invalid syntax.
+ * @apiError (Error 400) {Number} statusCode The status code
+ *
+ * @apiError (Error 401) {String} error The user doesn't have permission to do this action
+ * @apiError (Error 401) {Number} statusCode The status code
+ *
+ *
+ * @apiError (Error 404) {String} error Informs the caller of the missing object.
+ * @apiError (Error 404) {Number} statusCode The status code
+ *
+ * @apiErrorExample {json} Error-404:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "Group Not Found",
+ *        "status_code":404
+ *     }
+ *
+ * @apiErrorExample {json} Error-401:
+ *     HTTP/1.1 401 Unauthorized
+ *     {
+ *       "error": "Unauthorized User",
+ *        "status_code":401
+ *     }
+ * 
+ */
