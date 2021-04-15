d2e94616eb86e0fcf94386741d3a8b3b3b289a2f
Abdelrhman-Hosny <ahosny2792@gmail.com>
2021-04-15 07:41:05 +0200

    Added groups.member.getList (no Error or example)

---
+++ docs/groups/members/getList.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @api {GET} /groups/members/getList getList
+ * @apiGroup groups.members
+ * @apiVersion  1.0.0
+ * 
+ * @apiDescription Get a list of the members of a group.
+ * 
+ * @apiParam  {String} api_key Your API application key.
+ * @apiParam  {String} group_id The group that we want to get the members of.
+ * @apiParam  {Number} [memberTypes] Comma separated list of member types. 1: member, 2: moderator, 3:admin.
+ * @apiParam  {Number} [per_page=100] Number of members per page.
+ * @apiParam  {Number} [page=1] The page of results to return.
+ * 
+ * //TODO : Add request example with the list thing
+ * // i.e. the first optional argument.
+ * 
+ */
