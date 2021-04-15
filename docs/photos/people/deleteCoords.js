c9146f3494698cecbb6638380355a9cdd4b156c3
Abdelrhman-Hosny <ahosny2792@gmail.com>
2021-04-15 07:43:12 +0200

    Add photos.people.deleteCoord (no err or example)

---
+++ docs/photos/people/deleteCoords.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @api {DELETE} /photos/deleteCoords deleteCoords
+ * @apiGroup photos.people
+ * @apiVersion  1.0.0
+ * 
+ * @apiDescription Delete the bounding box of a person from the photo.
+ * 
+ * @apiParam  {String} api_key Your API application key
+ * @apiParam  {String} photo_id The id of the photo to add a person to.
+ * @apiParam  {String} user_id The id of the user to add to the photo.
+ * 
+ */
