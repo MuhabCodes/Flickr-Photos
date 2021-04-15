d4da3cb808b5d2151731573c58c082ce729fa3ad
mohamedafifi13 <61080870+mohamedafifi13@users.noreply.github.com>
2021-04-15 16:08:14 +0200

    Afifi Updated ApiDocs

---
+++ docs/photos.comments/comments.getList.js
@@ -0,0 +1,36 @@
+/**
+ * 
+ * @api {GET} /photos.comments/:photo_id getList
+ * @apiGroup photos.comments
+ * @apiVersion 1.0.0
+ * @apiDescription Returns the comments for a photo
+ * 
+ * 
+ * @apiParam  {String} api_key Your API application key. 
+ * @apiParam  {String} photo_id The id of the photo to fetch comments for.
+ * @apiParam  {Date} [min_comment_date] Minimum date that a a comment was added. The date should be in the form of a unix timestamp.
+ * @apiParam  {Date} [max_comment_date] Maximum date that a comment was added. The date should be in the form of a unix timestamp.
+ *
+ *@apiSuccess (Success 200) {Object[]} commentsList List of all Comments 
+*@apiSuccess (Success 200) {Number} statusCode The status code
+*@apiError (Error 400) {Number} statusCode The status code
+*@apiError (Error 400) {String} error The server could not understand the request due to invalid syntax.
+*@apiError (Error 401) {String} error The user doesn't have permission to do this action
+*@apiError (Error 401) {Number} statusCode The status code
+*@apiError (Error 404) {String} error Informs the caller of the missing object.
+*@apiError (Error 404) {Number} statusCode The status code
+*@apiErrorExample {json} Error-404
+*     HTTP/1.1 404 Not Found
+*     {
+*       "error": "photoNotFound",
+*        "status_code":404
+*     }
+*@apiErrorExample {json} Error-401
+*     HTTP/1.1 401 Unauthorized
+*     {
+*       "error": "Unauthorized User",
+*        "status_code":401
+*     }
+* 
+* 
+*/
---
+++ docs/photos.comments/getRecentForContacts.js
@@ -0,0 +1,37 @@
+/**
+ * 
+ * @api {GET} /photos.comments/getRecentForContacts
+
+* @apiGroup photos.comments
+* @apiVersion  1.0.0
+* 
+* 
+* @apiParam  {String} api_key Your API application key
+* @apiParam  {Date} [date_lastcomment] Limits the resultset to photos that have been commented on since this date. The date should be in the form of a Unix timestamp.
+* @apiParam  {Number} [per_page] Number of photos to return per page. If this argument is omitted, it defaults to 100. The maximum allowed value is 500.
+* @apiParam  {Number} [page] The page of results to return. If this argument is omitted, it defaults to 1.
+*@apiDescription Return the list of photos belonging to your contacts that have been commented on recently.
+*@apiSuccess (Success 200) {Object[]}  recentsList List of photos favorited by your contacts 
+*@apiSuccess (Success 200) {Number} statusCode The status code
+*@apiError (Error 400) {Number} statusCode The status code
+*@apiError (Error 400) {String} error The server could not understand the request due to invalid syntax.
+*@apiError (Error 401) {String} error The user doesn't have permission to do this action
+*@apiError (Error 401) {Number} statusCode The status code
+*@apiError (Error 404) {String} error Informs the caller of the missing object.
+*@apiError (Error 404) {Number} statusCode The status code
+ 
+*@apiErrorExample {json} Error-404
+*     HTTP/1.1 404 Not Found
+*     {
+*       "error": "photoNotFound",
+*        "status_code":404
+*     }
+*@apiErrorExample {json} Error-401
+*     HTTP/1.1 401 Unauthorized
+*     {
+*       "error": "Unauthorized User",
+*        "status_code":401
+*     }
+ * 
+ * 
+ */
