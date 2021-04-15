cc5a5ed1a78e900b831fecc98da656d74e686120
Abdelrhman-Hosny <ahosny2792@gmail.com>
2021-04-15 07:44:03 +0200

    Added photos.people.getList (no err or example)

---
+++ docs/photos/people/getList.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @api {GET} /photos/people/getList getPeopleList
+ * @apiGroup photos.people
+ * @apiVersion  1.0.0
+ * 
+ * @apiDescription Get a list of people in a given photo.
+ * 
+ * @apiParam  {String} api_key Your API application key.
+ * @apiParam  {String} photo_id The id of the photo.
+ * 
+ */
