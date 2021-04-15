c2601af88849b97029c58d9bb9d0b2faef321214
Abdelrhman-Hosny <ahosny2792@gmail.com>
2021-04-15 11:57:39 +0200

    added some errors and success to file

--- docs/groups/members/getList.js
+++ docs/groups/members/getList.js
@@ -15,7 +15,34 @@
  * //TODO : Add request example with the list thing
  * // i.e. the first optional argument.
  * 
- * @apiSuccess (Successful Requests) {String[]} 200:OK Contains all the member of the group mentioned
+ * @apiErrorExample {json} Error-404:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "PhotoNotFound",
+ *        "status_code":404
+ *     }
+ *
+ *     @apiErrorExample {json} Error-404:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "PhotoNotFound",
+ *        "status_code":404
+ *     }
+ * @apiErrorExample {json} Error-404:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "PhotoNotFound",
+ *        "status_code":404
+ *     }
  * 
- * @apiError  400:BadRequest The server could not understand the request due to invalid syntax.
+ *  @apiError (Error 404) {Number} status_code The status code
+ *  @apiError (Error 404) {String} PhotoNotFound The photo isn't found
+ * 
+ * @apiError (Error 401) {Number} status_code The status code
+ * @apiError (Error 401) {String} error The user doesn't have persmission to do this action
+ * 
+ * 
+ * @apiSuccess (Success 200) {Number} StatusCode all the member of the group mentioned
+ * @apiSuccess (Success 200) {String[]} memberList all the member of the group mentioned
+ *  
  */
