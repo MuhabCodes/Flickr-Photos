4fb35345fdef43024678cb6e48faa58e563bfe2d
ahmedehabb <Ahmed.bahloul00@eng-st.cu.edu.eg>
2021-04-19 14:52:39 +0200

    Photos.geo/ errorCodes+example

--- docs/photos/geo/photosForLocation.js
+++ docs/photos/geo/photosForLocation.js
@@ -22,14 +22,37 @@
  *  
  * @apiError (Error 400) {Number} statusCode The status code
  * @apiError (Error 400) {String} error The server could not understand the request due to invalid syntax.
- 
- 
-@apiError (Error 401) {String} error The user doesn't have permission to do this action
-*@apiError (Error 401) {Number} statusCode The status code
-
- 
-@apiError (Error 404) {String} error Informs the caller of the missing object.
-*@apiError (Error 404) {Number} statusCode The status code
+ *
+ *
+ * @apiError (Error 401) {String} error The user doesn't have permission to do this action
+ * @apiError (Error 401) {Number} statusCode The status code
+ *
+ * @apiError (Error 403) {String} Forbidden The server understood the request but refuses to authorize it.
+ * @apiError (Error 403) {Number} statusCode The status code
+ *
+ *
+ * @apiError (Error 404) {String} error Informs the caller of the missing object.
+ * @apiError (Error 404) {Number} statusCode The status code
+ * 
+ * 
+ * @apiError (Error 403) {String} Forbidden The server understood the request but refuses to authorize it.
+ * @apiError (Error 403) {Number} statusCode The status code
+ *
+ * 
+ * @apiError (Error 408) {String} Request Timeout; the server would like to shut down this unused connection. It is sent on an idle connection by some servers, even without any previous request by the client.
+ * @apiError (Error 408) {Number} statusCode The status code
+ * 
+ * @apiError (Error 417) {String} Expectation Failed ; the expectation given in the request's Expect header could not be met.
+ * @apiError (Error 417) {Number} statusCode The status code
+ * 
+ * @apiError (Error 429) {String} Too Many Requests ; he user has sent too many requests in a given amount of time ("rate limiting").
+ * @apiError (Error 429) {Number} statusCode The status code
+ * 
+ * @apiError (Error 411) {String} Length required ; the server refuses to accept the request without a defined Content-Length header.
+ * @apiError (Error 411) {Number} statusCode The status code 
+ *
+ * @apiError (Error 500) {String} Internal Server Error ;the server encountered an unexpected condition that prevented it from fulfilling the request.
+ * @apiError (Error 500) {Number} statusCode The status code
  
 @apiErrorExample {json} Error-404
 *     HTTP/1.1 404 Not Found
@@ -44,9 +67,37 @@
 *       "error": "Unauthorized User",
 *        "statusCode":401
 *     }
+*
 
-
-
+*@apiSuccessExample {json} Success-Response:
+*{
+*   "page": "2",
+*   "pages": "89",
+*   "perPage": "10",
+*   "total": "881",
+*   "photo": [
+*      {
+*         "id": "2636",
+*         "owner": "47058503995@N01",
+*         "secret": "a123456",
+*         "server": "2",
+*         "title": "test_04",
+*         "isPublic": "1",
+*         "isFriend": "0",
+*         "isFamily": "0"
+*      },
+*      {
+*         "id": "2635",
+*         "owner": "47058503995@N01",
+*         "secret": "b123456",
+*         "server": "2",
+*         "title": "test_03",
+*         "isPublic": "0",
+*         "isFriend": "1",
+*         "isFamily": "1"
+*      }
+*    ]
+*}
 
  * 
  * 
