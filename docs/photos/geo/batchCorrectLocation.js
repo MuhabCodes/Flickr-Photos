805a45e4c8d2e9554238696ec8d0bcfd9528032b
ahmedehabb <Ahmed.bahloul00@eng-st.cu.edu.eg>
2021-04-20 00:25:13 +0200

    Error names

--- docs/photos/geo/batchCorrectLocation.js
+++ docs/photos/geo/batchCorrectLocation.js
@@ -17,14 +17,14 @@ Batch corrections are processed in a delayed queue so it may take a few minutes 
  * 
  * @apiSuccess (Success 200) {Number} statusCode The status code
  * 
- * @apiError (Error 400) {Number} statusCode The status code
+  * @apiError (Error 400) {Number} statusCode The status code
  * @apiError (Error 400) {String} error The server could not understand the request due to invalid syntax.
  *
  *
  * @apiError (Error 401) {String} error The user doesn't have permission to do this action
  * @apiError (Error 401) {Number} statusCode The status code
  *
- * @apiError (Error 403) {String} Forbidden The server understood the request but refuses to authorize it.
+ * @apiError (Error 403) {String} error Forbidden The server understood the request but refuses to authorize it.
  * @apiError (Error 403) {Number} statusCode The status code
  *
  *
@@ -32,23 +32,22 @@ Batch corrections are processed in a delayed queue so it may take a few minutes 
  * @apiError (Error 404) {Number} statusCode The status code
  * 
  * 
- *
- * 
- * @apiError (Error 408) {String} Request Timeout; the server would like to shut down this unused connection. It is sent on an idle connection by some servers, even without any previous request by the client.
+ * @apiError (Error 408) {String} error RequestTimeout the server would like to shut down this unused connection. It is sent on an idle connection by some servers, even without any previous request by the client.
  * @apiError (Error 408) {Number} statusCode The status code
  * 
- * @apiError (Error 417) {String} Expectation Failed ; the expectation given in the request's Expect header could not be met.
+ * @apiError (Error 411) {String} error Length required the server refuses to accept the request without a defined Content-Length header.
+ * @apiError (Error 411) {Number} statusCode The status code 
+ * 
+ * @apiError (Error 417) {String} error Expectation Failed the expectation given in the request's Expect header could not be met.
  * @apiError (Error 417) {Number} statusCode The status code
  * 
- * @apiError (Error 429) {String} Too Many Requests ; he user has sent too many requests in a given amount of time ("rate limiting").
+ * @apiError (Error 429) {String} error Too Many Requests he user has sent too many requests in a given amount of time ("rate limiting").
  * @apiError (Error 429) {Number} statusCode The status code
  * 
- * @apiError (Error 411) {String} Length required ; the server refuses to accept the request without a defined Content-Length header.
- * @apiError (Error 411) {Number} statusCode The status code 
  *
- * @apiError (Error 500) {String} Internal Server Error ;the server encountered an unexpected condition that prevented it from fulfilling the request.
+ * @apiError (Error 500) {String} error Internal Server Error the server encountered an unexpected condition that prevented it from fulfilling the request.
  * @apiError (Error 500) {Number} statusCode The status code
- * 
+* 
  *@apiErrorExample {json} Error-404
  *     HTTP/1.1 404 Not Found
  *     {
