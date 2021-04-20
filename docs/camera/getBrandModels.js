804bae3d553fa93f6f575aad2420c8cb6f203d43
yousefQadry <58371826+yousefQadry@users.noreply.github.com>
2021-04-20 16:57:37 +0200

    Remaining errors in acitivity,camera,places

--- docs/camera/getBrandModels.js
+++ docs/camera/getBrandModels.js
@@ -35,10 +35,12 @@
  * 
  * 
  * 
- * @apiError (Error 401) {String} error The user doesn't have persmission to do this action
- * @apiError (Error 401) {Number} statusCode The status code
- * @apiError (Error 404) {String} error Informs the caller of the missing object.
- * @apiError (Error 404) {Number} statusCode The status code 
+ * @apiError (Error 404) {String} error brand not found at our application
+ * @apiError (Error 404) {Number} statusCode The status code.
+ * 
+ * 
+ * @apiError (Error 500) {String} error The server has encountered a situation it doesn't know how to handle.
+ * @apiError (Error 500) {Number} statusCode The status code
  * @apiErrorExample {json} Error-404:
  *     HTTP/1.1 404 Not Found
  *     {
@@ -46,12 +48,6 @@
  *        "statusCode":404
  *     }
   
- * @apiErrorExample {json} Error-401:
- *     HTTP/1.1 401 Unauthorized
- *     {
- *       "error": "Unauthorized User",
- *        "statusCode":401
- *     }
 
  * 
  */
--- docs/places/getInfoByUrl.js
+++ docs/places/getInfoByUrl.js
@@ -40,10 +40,16 @@
    }
    
 }
- * @apiError (Error 401) {String} error The user doesn't have persmission to do this action
- * @apiError (Error 401) {Number} statusCode The status code
- * @apiError (Error 404) {String} error Informs the caller of the missing object.
- * @apiError (Error 404) {Number} statusCode The status code 
+ * @apiError (Error 400) {String} error The format for the url isn't correct.
+ * @apiError (Error 400) {Number} statusCode The status code.
+ * 
+ * @apiError (Error 404) {String} error place not found at our application
+ * @apiError (Error 404) {Number} statusCode The status code.
+ * 
+ * 
+ * @apiError (Error 500) {String} error The server has encountered a situation it doesn't know how to handle.
+ * @apiError (Error 500) {Number} statusCode The status code
+ 
  * @apiErrorExample {json} Error-404:
  *     HTTP/1.1 404 Not Found
  *     {
@@ -51,12 +57,6 @@
  *        "statusCode":404
  *     }
   
- * @apiErrorExample {json} Error-401:
- *     HTTP/1.1 401 Unauthorized
- *     {
- *       "error": "Unauthorized User",
- *        "statusCode":401
- *     }
  * 
  * 
  * 
