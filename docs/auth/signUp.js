de1ee56b0dd50a20e7888e10c4331cfdf9fcccc0
Abdelrhman-Hosny <ahosny2792@gmail.com>
2021-04-20 09:12:22 +0200

    added signup

--- docs/auth/signUp.js
+++ docs/auth/signUp.js
@@ -4,7 +4,7 @@
  * @apiGroup auth
  * @apiVersion  0.1.0
  * 
- * @apiDescription This request checks tha
+ * @apiDescription This request sends an email to the email supplied to validate it and if validated a user is created.
  * 
  * @apiParam  {String} firstName First name of person
  * @apiParam  {String} lastName Last name of person
@@ -14,8 +14,15 @@
  * @apiParam  {String} password Password of the new account
  * 
  * 
- * @apiError (Error 409) {String} error The server has encountered a situation it doesn't know how to handle.
+ * @apiError (Error 400) {String} error The server could not understand the request due to invalid syntax.
+ * @apiError (Error 400) {Number} statusCode The status code
+ * 
+ * 
+ * @apiError (Error 409) {String} error The email you are trying to use is already registered
  * @apiError (Error 409) {Number} statusCode The status code
+ * 
+ * @apiError (Error 422) {String} error The request was well-formed but was unable to be followed due to semantic errors like invalid password or age ..etc.
+ * @apiError (Error 422) {Number} statusCode The status code
  * 
  * @apiError (Error 500) {String} error The server has encountered a situation it doesn't know how to handle.
  * @apiError (Error 500) {Number} statusCode The status code
