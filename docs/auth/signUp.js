1977cd977cc619553ddfe76ad8f03a31f0aab4f1
Abdelrhman-Hosny <ahosny2792@gmail.com>
2021-04-20 09:00:50 +0200

    Fixed a typo

---
+++ docs/auth/signUp.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ * @api {POST} /auth/signUp signUp
+ * @apiGroup auth
+ * @apiVersion  0.1.0
+ * 
+ * @apiDescription This request checks tha
+ * 
+ * @apiParam  {String} firstName First name of person
+ * @apiParam  {String} lastName Last name of person
+ * @apiParam  {Number} age Age of person
+ * @apiParam  {String} firstName First name of person
+ * @apiParam  {String} email Email address of the person
+ * @apiParam  {String} password Password of the new account
+ * 
+ * 
+ * @apiError (Error 409) {String} error The server has encountered a situation it doesn't know how to handle.
+ * @apiError (Error 409) {Number} statusCode The status code
+ * 
+ * @apiError (Error 500) {String} error The server has encountered a situation it doesn't know how to handle.
+ * @apiError (Error 500) {Number} statusCode The status code
+ * 
+ * 
+ * 
+ */
