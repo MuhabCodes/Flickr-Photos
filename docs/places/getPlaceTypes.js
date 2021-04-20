034b48a66e4dc74d85861c25ef4ffca4b7d30e5f
yousefQadry <58371826+yousefQadry@users.noreply.github.com>
2021-04-20 02:47:21 +0200

    Removed woe,modified our placetype id's so that city=8, country=12, continent = 29

--- docs/places/getPlaceTypes.js
+++ docs/places/getPlaceTypes.js
@@ -11,6 +11,21 @@
  * 
  * @apiSuccess (Success 200) {Number} statusCode The status code
  * @apiSuccess (Success 200) {Object[]} placeTypes list of all available place types 
+ * @apiSuccessExample {json} Success-Response:
+*    [
+  {
+     "placeTypeId": "8",
+     "text": "city"
+  },
+  {
+     "placeTypeId": "12",
+     "text": "country"
+  },
+  {
+     "placeTypeId": "29",
+     "text": "continent"
+  }
+]
  * 
  * @apiError (Error 401) {String} error The user doesn't have persmission to do this action
  * @apiError (Error 401) {Number} statusCode The status code
--- docs/places/getTopPlacesList.js
+++ docs/places/getTopPlacesList.js
@@ -21,10 +21,25 @@
  * 
  * @apiParam  {String} [date="yesterday"] A valid date in YYYY-MM-DD format.
  * @apiParam  {String} [placeId] Limit your query to only those top places belonging to a specific Flickr Places identifier.
- * @apiParam  {String} [woeId] Limit your query to only those top places belonging to a specific Where on Earth (WOE) identifier.
- * 
  * @apiSuccess (Success 200) {Number} statusCode The status code
  * @apiSuccess (Success 200) {Object[]} geoPlacesTags list of top 100 geotagged places for a day.
+ * @apiSuccessExample {json} Success-Response:
+ *    {
+   "total": "100",
+   "dateStart": "1246320000",
+   "dateStop": "1246406399",
+   "place": {
+      "placeId": "4KO02SibApitvSBieQ",
+      
+      "latitude": "48.890",
+      "longitude": "-116.982",
+      "placeUrl": "/United+States",
+      "placeType": "country",
+      "placeTypeId": "12",
+      "photoCount": "23371",
+      "text": "United States"
+   }
+}
  * 
  * @apiError (Error 401) {String} error The user doesn't have persmission to do this action
  * @apiError (Error 401) {Number} statusCode The status code
