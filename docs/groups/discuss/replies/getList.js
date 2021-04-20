83186ee7b38c727222df2e4c77df5c7af6db5038
mohamedafifi13 <61080870+mohamedafifi13@users.noreply.github.com>
2021-04-20 16:09:53 +0200

    error typo

--- docs/groups/discuss/replies/getList.js
+++ docs/groups/discuss/replies/getList.js
@@ -22,15 +22,15 @@
  * @apiError (Error 401) {Number} statusCode The status code
  * 
  
- *  * @apiError (Error 403) {String} Forbidden The server understood the request but refuses to authorize it.
+ *  * @apiError (Error 403) {String} error Forbidden The server understood the request but refuses to authorize it.
 * @apiError (Error 403) {Number} statusCode The status code
 * @apiError (Error 404) {String} error Informs the caller of the missing object.
  * @apiError (Error 404) {Number} statusCode The status code.
 
 
-* @apiError (Error 429) {String} Too Many Requests ; the user has sent too many requests in a given amount of time ("rate limiting").
+* @apiError (Error 429) {String} errorToo Many Requests ; the user has sent too many requests in a given amount of time ("rate limiting").
 * @apiError (Error 429) {Number} statusCode The status code
-* @apiError (Error 500) {String} The server has encountered a situation it doesn't know how to handle.
+* @apiError (Error 500) {String} error The server has encountered a situation it doesn't know how to handle.
 * @apiError (Error 500) {Number} statusCode The status code
 
  * 
