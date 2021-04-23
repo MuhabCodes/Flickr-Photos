970df53813d7b26f73418d05533e33baaaead3a5
Abdelrhman-Hosny <ahosny2792@gmail.com>
2021-04-23 21:36:22 +0200

    added connection to mongo

--- Backend/bin/server.js
+++ Backend/bin/server.js
@@ -1,8 +1,13 @@
 const { resolve } = require('path');
 require('dotenv').config({ path: resolve('../secrets/', '.env') });
+const mongoose = require('mongoose');
 
 // DB Connection
-
+mongoose
+  .connect('mongodb://mongo:27017/docker-node-mongo',
+    { useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.log(err));
 // Server Connection
 const http = require('http');
 const app = require('../app');
