35454d7fba818d3738ae328ae5936b84728a79c6
Abdelrhman-Hosny <ahosny2792@gmail.com>
2021-04-24 17:54:56 +0200

    add skeleton for auth router and controller

---
+++ Backend/components/auth/authController.js
@@ -0,0 +1,23 @@
+const userModel = require('../User/userModel');
+
+exports.login = async function loginUser(req, res, next) {
+  const { email, password } = req.body;
+
+  try {
+
+  } catch (error) {
+
+  }
+};
+
+exports.register = async function registerUser(req, res, next) {
+  const {
+    firstName, lastName, age, email, password,
+  } = req.body;
+
+  try {
+
+  } catch (error) {
+
+  }
+};
---
+++ Backend/components/auth/authRouter.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+
+const authRouter = Router();
+
+authRouter.route('/login').post((req, res) => {
+  // placeholder
+});
+
+authRouter.route('/register').post((res, req) => {
+  // placeholder
+});
