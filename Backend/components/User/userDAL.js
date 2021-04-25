2be2cac14e68696ffc56db103ad5881baf0ed935
Abdelrhman-Hosny <ahosny2792@gmail.com>
2021-04-25 23:36:51 +0200

    add createNewUser

--- Backend/components/User/userDAL.js
+++ Backend/components/User/userDAL.js
@@ -1,6 +1,24 @@
-const user = require('./userModel');
+const bcrypt = require('bcrypt');
+const User = require('./userModel');
 
-exports.getUserByEmail = async function getWithEmail(email) {
-  const userObj = user.findOne({ email });
+exports.getUserByEmail = async function getWithEmail({ email }) {
+  const userObj = await User.findOne({ email });
   return userObj;
+};
+
+exports.createNewUser = async function createUser({
+  email, password, firstName, lastName, age,
+}, next) {
+  try {
+    // password encryption
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const userObj = new User({
+      email,
+      password: hashedPassword, // TODO : add Person Id with firstName, lastName and age
+    });
+    // Adding user to db
+    await userObj.save();
+  } catch (err) {
+    next(err);
+  }
 };
