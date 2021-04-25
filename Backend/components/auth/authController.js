d4b64988c5757235317886bbccf85923721ce1b2
Abdelrhman-Hosny <ahosny2792@gmail.com>
2021-04-25 23:42:26 +0200

    add register in authController

--- Backend/components/auth/authController.js
+++ Backend/components/auth/authController.js
@@ -1,20 +1,29 @@
 const userDAL = require('../User/userDAL');
 
-exports.login = async function loginUser(req, res, next) {
-  const { email, password } = req.body;
-
+exports.login = async function loginUser({ email, password }) {
   try {
     // placeholder
+    console.log(email, password);
   } catch (error) {
     // placeholder
   }
 };
 
-exports.register = async function registerUser(body) {
-  const user = await userDAL.getUserByEmail(body.email);
+exports.register = async function registerUser(
+  req, res, next,
+) {
+  const { body } = req;
+  const user = await userDAL.getUserByEmail(body);
   if (!user) {
     // create user after checking for email and password
+    try {
+      await userDAL.createNewUser(body);
+      res.status(201).send({ statusCode: 201 });
+    } catch (err) {
+      res.status(500).send({ statusCode: 500, error: 'The server couldn\'t handle the registration process' });
+    }
   } else {
     // say that an email is sent but don't send for security purposes
+    next();
   }
 };
