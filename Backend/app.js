1f9381434a5cc70061d31ca0b70bd304b7a519ce
Abdelrhman-Hosny <ahosny2792@gmail.com>
2021-04-25 23:39:52 +0200

    add auth Router to app

--- Backend/app.js
+++ Backend/app.js
@@ -1,17 +1,20 @@
 const express = require('express');
-
+const cors = require('cors');
 // require routes only here
+const authRouter = require('./components/auth/authRouter');
 
 // declaring app
 const app = express();
 
 // middleware here (no routing)
 app.use(express.json());
+app.use(cors());
 
 // use routing i.e. app.use('foo', bar)
 app.route('/').get((req, res) => {
   res.send('Hello world!');
 });
+app.use('/auth', authRouter);
 
 // exporting
 module.exports = app;
