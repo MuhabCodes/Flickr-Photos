da334821f87acd9b0ae68f1a777a8d3f8483d7d2
AhmedMAyoub <ahmed2000mostafa@gmail.com>
2021-04-26 13:58:29 +0200

    Added HomePage Filters Div

--- FrontEnd/flickr-photos/src/Home/HomePage.jsx
+++ FrontEnd/flickr-photos/src/Home/HomePage.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 
 const HomePage = () => {
-//   const { data, isPending, error } = 1;
+  //   const { data, isPending, error } = 1;
   const data = 1;
   console.log(data);
   return (
-    <div className="homePageMain">
-      <div className="homePageFeed">This is Feed</div>
-      <div className="homePageSideBar">This is SideBar</div>
+    <div className="homePageContentMain">
+      <div className="homePageFeed">
+        <div className="homeFeedFilters">
+          <div className="homeFeedDropDown">
+            ad
+          </div>
+          <div className="homeFeedLayouts">
+            ad
+          </div>
+        </div>
+      </div>
+      <div className="homePageSideBar">
+        This is SideBar
+        <img
+          src="https://static.toiimg.com/photo/72975551.cms"
+          alt="Moon"
+          style={{ maxWidth: '100%', maxHeight: '100%' }}
+        />
+      </div>
     </div>
   );
 };
