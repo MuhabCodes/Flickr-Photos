60e5bf2f139ffa898282d2682eba9e594f0689eb
SandySamir236 <63124393+SandySamir236@users.noreply.github.com>
2021-04-26 11:52:34 +0200

    Navbar items
    Navbar items without dropdown list

--- FrontEnd/flickr-photos/src/App/App.jsx
+++ FrontEnd/flickr-photos/src/App/App.jsx
@@ -1,28 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+import { Navbar, Nav } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar sticky="top" expand="lg">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <nav className="collapse navbar-collapse  " id="basic-navbar-nav">
+            <Nav.Link href="#home">You</Nav.Link>
+            <Nav.Link href="#link">Explore</Nav.Link>
+            <Nav.Link href="#link">Print</Nav.Link>
+            <Nav.Link href="#link">Get Pro</Nav.Link>
+          </nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
