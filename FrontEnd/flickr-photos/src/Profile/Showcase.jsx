faa8f72260a65264ca339012ba95ec821c91ee82
manaarr <manaarrzakaria@gmail.com>
2021-04-27 01:23:28 +0200

    structure of showcase

---
+++ FrontEnd/flickr-photos/src/Profile/Showcase.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Showcase = () => (
+  <div className="showcase-view">
+    <div className="showcase">
+      <div className="title-container">
+        <p className="title-view">ShowCase</p>
+      </div>
+      <div className="photo-list-view">
+        <img src="./assets/shapes_heart.png" alt="" />
+      </div>
+      <div className="divider" />
+    </div>
+  </div>
+);
+
+export default Showcase;
