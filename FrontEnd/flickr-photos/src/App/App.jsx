cc3db700845229f3be58b65efd50da636a12f497
SandySamir236 <63124393+SandySamir236@users.noreply.github.com>
2021-04-26 12:40:54 +0200

    Navbar brand with logo
    added font for the brand in the navbar and linked its font in index.html

--- FrontEnd/flickr-photos/src/App/App.jsx
+++ FrontEnd/flickr-photos/src/App/App.jsx
@@ -9,6 +9,18 @@ function App() {
     <div className="App">
       <Navbar sticky="top" expand="lg">
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src="flickr.png"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />
+          {' '}
+          flickr
+        </Navbar.Brand>
+
         <Navbar.Collapse id="basic-navbar-nav">
           <nav className="collapse navbar-collapse  " id="basic-navbar-nav">
             <Nav.Link href="#home">You</Nav.Link>
