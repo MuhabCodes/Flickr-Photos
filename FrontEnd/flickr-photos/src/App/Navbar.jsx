8a2a125d92ac0a1267143c537bb114b3306386c1
SandySamir236 <63124393+SandySamir236@users.noreply.github.com>
2021-04-26 13:55:25 +0200

    Navbar drop down

--- FrontEnd/flickr-photos/src/App/Navbar.jsx
+++ FrontEnd/flickr-photos/src/App/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 function NavBar() {
   return (
@@ -23,9 +23,36 @@ function NavBar() {
 
         <Navbar.Collapse id="basic-navbar-nav">
           <nav className="collapse navbar-collapse  " id="basic-navbar-nav">
-            <Nav.Link href="#home">You</Nav.Link>
-            <Nav.Link href="#link">Explore</Nav.Link>
-            <Nav.Link href="#link">Print</Nav.Link>
+            <NavDropdown title="You">
+              <NavDropdown.Item href="#action/3.1">About</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Photostream </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Albums</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Faves</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Galleries</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Groups</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Camera Roll</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.3">Recent Activity</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">People</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Organize</NavDropdown.Item>
+            </NavDropdown>
+
+            <NavDropdown title="Explore">
+              <NavDropdown.Item href="#action/3.1">Recent Photos</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Trending</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Events</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">The Commons</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Flicker Galleries</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">World Map</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Camera Finder</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Flickr Blog</NavDropdown.Item>
+            </NavDropdown>
+
+            <NavDropdown title="Print">
+              <NavDropdown.Item href="#action/3.1">Prints & Wall Art</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Photo Books</NavDropdown.Item>
+            </NavDropdown>
+
             <Nav.Link href="#link">Get Pro</Nav.Link>
           </nav>
         </Navbar.Collapse>
