89c072d33c1a9fd83d051641b13040ec462980d8
manaarr <manaarrzakaria@gmail.com>
2021-04-27 00:43:54 +0200

    used custom hook in profile container to fetch bio

--- FrontEnd/flickr-photos/src/Profile/ProfileContainer.jsx
+++ FrontEnd/flickr-photos/src/Profile/ProfileContainer.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import useFetch from './usefetch';
 import AboutBio from './AboutBio';
 
+const { data, isPending, Error } = useFetch('http://localhost:8000/aboutbio');
+
 const ProfileContainer = () => (
+
   <div className="fluid-profile-container">
     <div className="profile-bio-page">
       <div className="bio-description-section">
-        <AboutBio />
+        {Error && <div>{Error}</div>}
+        {isPending && <div>Loading...</div>}
+        {data && <AboutBio bios={data} />}
       </div>
     </div>
   </div>
