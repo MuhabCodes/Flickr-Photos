cc576672d50319e3745194341968c7f08debe50a
manaarr <manaarrzakaria@gmail.com>
2021-04-27 15:30:14 +0200

    adjusted profilecontainer with new user info

--- FrontEnd/flickr-photos/src/Profile/ProfileContainer.jsx
+++ FrontEnd/flickr-photos/src/Profile/ProfileContainer.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import useFetch from './usefetch';
 import AboutBio from './AboutBio';
+import Showcase from './Showcase';
+import UserInfo from './UserInfo';
 
-const { data, isPending, Error } = useFetch('http://localhost:8000/aboutbio');
+// const { data, isPending, Error } = useFetch('http://localhost:8000/aboutbio');
 
 const ProfileContainer = () => (
 
   <div className="fluid-profile-container">
     <div className="profile-bio-page">
       <div className="bio-description-section">
-        {Error && <div>{Error}</div>}
-        {isPending && <div>Loading...</div>}
-        {data && <AboutBio bios={data} />}
+        <AboutBio />
+        <Showcase />
+        <UserInfo />
       </div>
     </div>
   </div>
