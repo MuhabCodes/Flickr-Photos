cd763fff53b4a500630c53af83aeed8de8227c3a
YomnaTarek <yomnaragab13@gmail.com>
2021-04-26 23:04:23 -0700

    'More' has been aligned to the right
    'More' has been aligned to the right of the navbar.

--- FrontEnd/flickr-photos/src/RecentPhotos/ExploreNavBar.jsx
+++ FrontEnd/flickr-photos/src/RecentPhotos/ExploreNavBar.jsx
@@ -8,22 +8,24 @@ import {
 
 function ExploreNavBar() {
   return (
-    <Navbar bg="light" expand="lg" fixed="top">
+    <Navbar bg="white" expand="lg" fixed="top">
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
+        <Nav className="container-fluid">
           <Nav.Link href="#explore">Explore</Nav.Link>
           <Nav.Link href="#trending">Trending</Nav.Link>
           <Nav.Link href="#events">Events</Nav.Link>
-          <NavDropdown title="More" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">The Commons</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Galleries</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">World Map</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">App Garden</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Camera Finder</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">The Weekly Flickr</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Flickr Blog</NavDropdown.Item>
-          </NavDropdown>
+          <Nav.Item className="ml-auto">
+            <NavDropdown title="More" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">The Commons</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Galleries</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">World Map</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">App Garden</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Camera Finder</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">The Weekly Flickr</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Flickr Blog</NavDropdown.Item>
+            </NavDropdown>
+          </Nav.Item>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
