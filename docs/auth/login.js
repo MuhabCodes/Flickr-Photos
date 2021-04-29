39d4a88cf63b9abf70440d5b4ab929311fc54626
Abdelrhman-Hosny <ahosny2792@gmail.com>
2021-04-29 09:57:06 +0200

    modified error messages in login

--- docs/auth/login.js
+++ docs/auth/login.js
@@ -9,19 +9,16 @@
  * @apiParam  (Body) {String} email The email the user uses to login.
  * @apiParam  (Body) {String} password The password that corresponds to the email.
  * 
- * @apiSuccess (Success 200) {String} accessToken Token that is used for authentication and authorization of opertaions throughout Flickr
- * @apiSuccess (Success 200) {Number} statusCode Indicates the success of the operation.
+ * @apiSuccess (Success 201) {String} accessToken Token that is used for authentication and authorization of opertaions throughout Flickr
+ * @apiSuccess (Success 201) {Number} statusCode Indicates the success of the operation.
  * 
  * 
- * @apiError (Error 400) {String} error The server could not understand the request due to invalid syntax.
- * @apiError (Error 400) {Number} statusCode The status code.
+ * @apiError (Error 401) {String} error The email or password entered were not correct.
+ * @apiError (Error 401) {Number} statusCode The status code
  * 
- * @apiError (Error 401) {String} error The client must authenticate itself to get the requested response.( client unknown to server)
- * @apiError (Error 401) {Number} statusCode The status code.
+ * @apiError (Error 403) {String} error This client hasn't activated their account.
+ * @apiError (Error 403) {Number} statusCode The status code
  * 
- * @apiError (Error 404) {String} error The server can not find the requested resource. 
- * @apiError (Error 404) {Number} statusCode The status code.
- *
  * 
  * @apiError (Error 500) {String} error The server has encountered a situation it doesn't know how to handle.
  * @apiError (Error 500) {Number} statusCode The status code
