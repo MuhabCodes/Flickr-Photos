b29e475d865846f4500d374c692f3a62fd7877d6
SandySamir236 <63124393+SandySamir236@users.noreply.github.com>
2021-05-01 16:03:05 +0200

    navbar
    till here the navbar is responsive but have a problem and the footer is done with its responsive behaviour

--- FrontEnd/flickr-photos/src/App/NavbarDropDown.jsx
+++ FrontEnd/flickr-photos/src/App/NavbarDropDown.jsx
@@ -14,34 +14,34 @@ function NavBarDropDown() {
       <div>
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto ">
-            <NavDropdown title="You" id="collasible-nav-dropdown">
+            <NavDropdown title="You" id="collasible-nav-dropdown" data-bs-toggle="dropdown">
               <NavDropdown.Item href="#action/3.1">About</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Photostream </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Albums</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Faves</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Galleries</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Groups</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Camera Roll</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Faves</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.5">Galleries</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.6">Groups</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.7">Camera Roll</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.3">Recent Activity</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">People</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Organize</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.8">Recent Activity</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.9">People</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.10">Organize</NavDropdown.Item>
             </NavDropdown>
 
             <NavDropdown title="Explore">
-              <NavDropdown.Item href="#action/3.1">Recent Photos</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Trending</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Events</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">The Commons</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Flicker Galleries</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">World Map</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Camera Finder</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Flickr Blog</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.11">Recent Photos</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.12">Trending</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.13">Events</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.14">The Commons</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.15">Flicker Galleries</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.16">World Map</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.17">Camera Finder</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.18">Flickr Blog</NavDropdown.Item>
             </NavDropdown>
 
             <NavDropdown title="Print">
-              <NavDropdown.Item href="#action/3.1">Prints & Wall Art</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Photo Books</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.19">Prints & Wall Art</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.20">Photo Books</NavDropdown.Item>
             </NavDropdown>
             <Nav.Link href="#link">Get Pro</Nav.Link>
 
