97243aee839de3f2d8aa0a773cb32e4ab50f2094
ahmedehabb <Ahmed.bahloul00@eng-st.cu.edu.eg>
2021-05-01 23:30:38 +0200

    before merge

--- Backend/bin/server.js
+++ Backend/bin/server.js
@@ -5,8 +5,10 @@ const mongoose = require('mongoose');
 // DB Connection
 mongoose
   .connect('mongodb+srv://ahmedehab:ahmedehab@cluster0.hyt1i.mongodb.net/ahmedehab?retryWrites=true&w=majority',
-  {useNewUrlParser: true, useUnifiedTopology: true})
+    { useNewUrlParser: true, useUnifiedTopology: true })
+  // eslint-disable-next-line no-console
   .then(() => console.log('MongoDB Connected'))
+  // eslint-disable-next-line no-console
   .catch((err) => console.log(err));
 // Server Connection
 const http = require('http');
