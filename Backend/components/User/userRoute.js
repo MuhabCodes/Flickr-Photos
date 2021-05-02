5be26fcb1fba7caa4591eca5e75438d4f8d25e3f
yousefQadry <58371826+yousefQadry@users.noreply.github.com>
2021-05-02 02:18:28 +0200

    updated the URL of get user by displayName

--- Backend/components/User/userRoute.js
+++ Backend/components/User/userRoute.js
@@ -2,6 +2,6 @@ const express = require('express');
 const userController = require('./userController');
 
 const router = express.Router();
-router.get('/uname/:userName', userController.getUserbyDisplayName);
-router.get('/email/:email',userController.getUserByEmail);
+router.get('/displayname/:displayName', userController.getUserbyDisplayName);
+router.get('/email/:email', userController.getUserByEmail);
 module.exports = router;
