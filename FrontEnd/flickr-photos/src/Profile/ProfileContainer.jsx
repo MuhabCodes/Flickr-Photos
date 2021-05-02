3b3510084e573040078b929a5709c40e9a6bd25b
manaarr <manaarrzakaria@gmail.com>
2021-05-02 08:39:57 +0200

    added stats to profile container

--- FrontEnd/flickr-photos/src/Profile/ProfileContainer.jsx
+++ FrontEnd/flickr-photos/src/Profile/ProfileContainer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AboutBio from './AboutBio';
+import Stats from './GeneralStats';
 import Showcase from './Showcase';
 import UserInfo from './UserInfo';
 
@@ -13,6 +14,7 @@ const ProfileContainer = () => (
         <AboutBio />
         <Showcase />
         <UserInfo />
+        <Stats />
       </div>
     </div>
   </div>
