5e76c0ab1d54c1ca90af7d21cee306609d99c6a3
manaarr <manaarrzakaria@gmail.com>
2021-05-03 06:49:40 +0200

    read more/less styling button

--- FrontEnd/flickr-photos/src/Profile/AboutBio.jsx
+++ FrontEnd/flickr-photos/src/Profile/AboutBio.jsx
@@ -30,19 +30,18 @@ const AboutBio = () => {
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet,
           nulla et dictum
-          interdum, nisi lorem egestas vitae scel
+          interdum, nisi lorem egestas vitae scel erisque enim ligula venenatis dolor.
+          Maecenas nisl est,
+          ultrices nec congue eget, sed ullamcorper ipsum dignissim ac. In at libero sed nunc
           <span id="dots">...</span>
           <span id="more">
-            erisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget,
-            auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Nunc sagittis dictum nisi,
-            sed ullamcorper ipsum dignissim ac. In at libero sed nunc
             venenatis imperdiet sed ornare turpis.
             Donec vitae dui eget tellus gravida venenatis.
             Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta.
           </span>
         </p>
+        <button type="button" onClick={Read} id="readbtn">Read more</button>
       </div>
-      <button type="button" onClick={Read} id="readbtn">Read more</button>
       <div className="divider" />
     </div>
   );
