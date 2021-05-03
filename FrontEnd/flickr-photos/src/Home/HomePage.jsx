8c45b57e5d29241f197c422f946a40c4592539d7
AhmedMAyoub <ahmed2000mostafa@gmail.com>
2021-05-03 16:39:57 +0200

    Added SideBar and Explore Card to Sidebar but without image grid

--- FrontEnd/flickr-photos/src/Home/HomePage.jsx
+++ FrontEnd/flickr-photos/src/Home/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PeopleCard from './PeopleCard';
+import SideBar from './SideBar';
 
 const HomePage = () => {
   //   const { data, isPending, error } = 1;
@@ -49,12 +50,7 @@ const HomePage = () => {
         </div>
       </div>
       <div className="homePageSideBar">
-        This is SideBar
-        <img
-          src="https://static.toiimg.com/photo/72975551.cms"
-          alt="Moon"
-          style={{ maxWidth: '100%', maxHeight: '100%' }}
-        />
+        <SideBar />
       </div>
     </div>
   );
--- FrontEnd/flickr-photos/src/Home/SideBar.jsx
+++ FrontEnd/flickr-photos/src/Home/SideBar.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './SideBar.css';
+import ExploreCard from './ExploreCard';
 
 const SideBar = () => (
   <div className="sideBarMain">
     <div className="explorePhotosCard" />
+    <ExploreCard />
     <div className="groupsForYouCard" />
     <div className="upgradeToProCard" />
   </div>
