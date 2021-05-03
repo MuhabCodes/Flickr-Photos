9d372dda6d944ed543619d6d811bf9a99399d3d6
AhmedMAyoub <ahmed2000mostafa@gmail.com>
2021-05-03 14:41:14 +0200

    Added SideBar.jsx and its css

---
+++ FrontEnd/flickr-photos/src/Home/SideBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SideBar.css';
+
+const SideBar = () => (
+  <div className="sideBarMain">
+    <div className="explorePhotosCard" />
+    <div className="groupsForYouCard" />
+    <div className="upgradeToProCard" />
+  </div>
+);
+
+export default SideBar;
