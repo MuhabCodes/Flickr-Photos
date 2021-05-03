3d87c9f739cb03967ddf72dfb5b3a64c3be59c2b
KareemMous <kareemmoustafa05@gmail.com>
2021-05-03 23:32:54 +0200

    Added Validations

--- FrontEnd/flickr-photos/src/Signup/Signup.jsx
+++ FrontEnd/flickr-photos/src/Signup/Signup.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React, { useState } from 'react';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -5,13 +6,16 @@ import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { useHistory } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import background from './background.jpg';
 import icon from './flickrlogo.png';
 
 // Styles Added to The inputs
 const CssTextField = withStyles({
   root: {
-    margin: 10,
+    // margin: 10,
     minWidth: 305,
     justifyContent: 'center',
     alignSelf: 'center',
@@ -24,10 +28,10 @@ const useStyles = makeStyles({
     flexDirection: 'column',
     // alignItems: "center",
     marginTop: 20,
-    padding: 20,
+    padding: 10,
     minWidth: 360,
     maxWidth: 360,
-    height: 700,
+    height: 720,
   },
   bullet: {
     display: 'inline-block',
@@ -36,7 +40,7 @@ const useStyles = makeStyles({
   },
   title: {
     fontSize: 16,
-    padding: 10,
+    padding: 20,
     color: 'black',
   },
   pos: {
@@ -44,7 +48,18 @@ const useStyles = makeStyles({
   },
 });
 
+const schema = yup.object().shape({
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+  password: yup.string().min(12).required(),
+  // age: yup.number().integer().positive().required(),
+  email: yup.string().email().required(),
+});
+
 export default function SignUp() {
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema),
+  });
   const classes = useStyles();
   // the use of the use state and set state functions
   // to save the changes made in each of this inputs
@@ -60,9 +75,9 @@ export default function SignUp() {
   //   let passwordError = "";
 
   // };
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  // };
+  const submitForm = () => {
+    history.push('/verifysignup');
+  };
 
   return (
     <div style={{
@@ -81,21 +96,25 @@ export default function SignUp() {
         <Typography className={classes.title} style={{ fontSize: '1.25rem' }} color="textSecondary" gutterBottom>
           Sign up for Flickr
         </Typography>
-        <form onSubmit={() => history.push('/verifysignup')}>
-          <CssTextField variant="outlined" style={{ justifyContent: 'center', alignSelf: 'center' }} required value={firstName} onChange={(e) => setFName(e.target.value)} label="First Name" />
-          <CssTextField variant="outlined" required value={lastName} onChange={(e) => setLName(e.target.value)} label="Last Name" />
-          <CssTextField variant="outlined" type="number" required value={age} onChange={(e) => setAge(e.target.value)} label="Your Age" />
-          <CssTextField variant="outlined" type="email" required value={email} onChange={(e) => setEmail(e.target.value)} label="Email Address" />
-          <CssTextField variant="outlined" type="password" required value={password} onChange={(e) => setPassword(e.target.value)} label="Password" />
-
+        <form onSubmit={handleSubmit(submitForm)}>
+          <CssTextField variant="outlined" {...register('firstName')} name="firstName" style={{ justifyContent: 'center', alignSelf: 'center' }} value={firstName} onChange={(e) => setFName(e.target.value)} label="First Name" />
+          <p style={{ color: 'red', fontSize: '12px', transition: '10s' }}>{errors.firstName && 'Required'}</p>
+          <CssTextField variant="outlined" {...register('lastName')} name="lastName" value={lastName} onChange={(e) => setLName(e.target.value)} label="Last Name" />
+          <p style={{ color: 'red', fontSize: '12px', transition: '10s' }}>{errors.lastName && 'Required'}</p>
+          <CssTextField variant="outlined" {...register('age')} name="Age" type="number" value={age} onChange={(e) => setAge(e.target.value)} label="Your Age" />
+          <p style={{ color: 'red', fontSize: '12px', transition: '10s' }}>{errors.age && 'Required'}</p>
+          <CssTextField variant="outlined" {...register('email')} name="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} label="Email Address" />
+          <span style={{ color: 'red', fontSize: '12px', transition: '10s' }}>{errors.email && 'Required'}</span>
+          <CssTextField variant="outlined" {...register('password')} name="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} label="Password" />
+          <p style={{ color: 'red', fontSize: '12px', transition: '10s' }}>{errors.password && 'Required Must be 12 Characters'}</p>
           <Button
             variant="contained"
             style={{
               minWidth: 290,
               color: 'white',
               backgroundColor: '#128fdc',
-              paddingTop: '0.5rem',
-              marginTop: '1.8rem',
+              paddingTop: '8px',
+              marginTop: '8px',
               font: 'inherit',
             }}
             disableElevation
@@ -105,7 +124,11 @@ export default function SignUp() {
           </Button>
         </form>
         <div style={{
-          fontSize: '0.875rem', color: '#898989', position: 'relative', alignSelf: 'center', bottom: '-10px',
+          paddingTop: '8px',
+          fontSize: '0.75rem',
+          color: '#898989',
+          position: 'relative',
+          alignSelf: 'center',
         }}
         >
 
@@ -116,7 +139,7 @@ export default function SignUp() {
           </p>
         </div>
         <div style={{
-          fontSize: '0.875rem', position: 'relative', alignSelf: 'center', bottom: '-25px',
+          fontSize: '0.875rem', position: 'relative', alignSelf: 'center',
         }}
         >
           <p>
