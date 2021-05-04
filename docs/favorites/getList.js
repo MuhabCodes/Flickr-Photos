d7f77c2d49396b0d7c14266e7ef70821b722b73a
mohamedafifi13 <61080870+mohamedafifi13@users.noreply.github.com>
2021-05-04 22:48:24 +0200

    Updated my documentation files

--- docs/favorites/getList.js
+++ docs/favorites/getList.js
@@ -16,34 +16,11 @@
  * 
  * @apiSuccess (Success 200) {Object[]} favoritesList List of all user favorites
  * @apiSuccess (Success 200) {Number} statusCode The status code
-  *@apiError (Error 400) {Number} statusCode The status code
-*@apiError (Error 400) {String} error The server could not understand the request due to invalid syntax.
-*@apiError (Error 401) {String} error The client must authenticate itself to get the requested response.( client unknown to server)
-*@apiError (Error 401) {Number} statusCode The status code
 
-
-*@apiError (Error 404) {String} error The server can not find the requested resource. 
-*@apiError (Error 404) {Number} statusCode The status code
-
-
-
-* @apiError (Error 429) {String} error Too Many Requests ; the user has sent too many requests in a given amount of time ("rate limiting").
-* @apiError (Error 429) {Number} statusCode The status code
 
 * @apiError (Error 500) {String} error The server has encountered a situation it doesn't know how to handle.
 * @apiError (Error 500) {Number} statusCode The status code
- * @apiErrorExample {json} Error-404
- *     HTTP/1.1 404 Not Found
- *     {
- *       "error": "photoNotFound",
- *        "statusCode":404
- *     }
- * @apiErrorExample {json} Error-401
- *     HTTP/1.1 401 Unauthorized
- *     {
- *       "error": "Unauthorized User",
- *        "statusCode":401
- *     }
+
  * * @apiSuccessExample {json} Success-Response:
  *    {
    "page": "2",
@@ -100,3 +77,29 @@
  * 
  * 
  */
+// *@apiError (Error 400) {Number} statusCode The status code
+// *@apiError (Error 400) {String} error The server could not understand the request due to invalid syntax.
+// *@apiError (Error 401) {String} error The client must authenticate itself to get the requested response.( client unknown to server)
+// *@apiError (Error 401) {Number} statusCode The status code
+
+
+// *@apiError (Error 404) {String} error The server can not find the requested resource. 
+// *@apiError (Error 404) {Number} statusCode The status code
+
+
+
+// * @apiError (Error 429) {String} error Too Many Requests ; the user has sent too many requests in a given amount of time ("rate limiting").
+// * @apiError (Error 429) {Number} statusCode The status code
+
+// * @apiErrorExample {json} Error-404
+// *     HTTP/1.1 404 Not Found
+// *     {
+// *       "error": "photoNotFound",
+// *        "statusCode":404
+// *     }
+// * @apiErrorExample {json} Error-401
+// *     HTTP/1.1 401 Unauthorized
+// *     {
+// *       "error": "Unauthorized User",
+// *        "statusCode":401
+// *     }
--- docs/favorites/getPublicList.js
+++ docs/favorites/getPublicList.js
@@ -4,7 +4,7 @@
  * 
  * @apiGroup favorites
  * @apiVersion  0.1.0
- * @apiDescription Returns a list of the user's favorite photos. Only photos which the calling user has permission to see are returned.
+ * @apiDescription Returns a list of the user public favorite photos. 
  * 
  *  
  * @apiParam  {String} userId The NSID of the user to fetch the favorites list for. If this argument is omitted, the favorites list for the calling user is returned.
@@ -15,34 +15,11 @@
  * 
 * @apiSuccess (Success 200) {Object[]} favoritesList List of all user favorites
 *@apiSuccess (Success 200) {Number} statusCode The status code
-*@apiError (Error 400) {Number} statusCode The status code
-*@apiError (Error 400) {String} error The server could not understand the request due to invalid syntax.
 
-*@apiError (Error 401) {String} error The client must authenticate itself to get the requested response.( client unknown to server)
-*@apiError (Error 401) {Number} statusCode The status code
-*@apiError (Error 404) {String} error The server can not find the requested resource. 
-*@apiError (Error 404) {Number} statusCode The status code
-
-
-
-* @apiError (Error 429) {String} error Too Many Requests ; the user has sent too many requests in a given amount of time ("rate limiting").
-* @apiError (Error 429) {Number} statusCode The status code
 
 * @apiError (Error 500) {String} error The server has encountered a situation it doesn't know how to handle.
 * @apiError (Error 500) {Number} statusCode The status code
-*@apiErrorExample {json} Error-404
-*     HTTP/1.1 404 Not Found
-*     {
-*       "error": "photoNotFound",
-*        "statusCode":404
-*     }
-* 
-@apiErrorExample {json} Error-401
-*     HTTP/1.1 401 Unauthorized
-*     {
-*       "error": "Unauthorized User",
-*        "statusCode":401
-*     }
+
 @apiSuccessExample {json} Success-Response:
 *    {
    "page": "2",
@@ -99,3 +76,29 @@
 * 
 * 
 */
+// *@apiError (Error 400) {Number} statusCode The status code
+// *@apiError (Error 400) {String} error The server could not understand the request due to invalid syntax.
+
+// *@apiError (Error 401) {String} error The client must authenticate itself to get the requested response.( client unknown to server)
+// *@apiError (Error 401) {Number} statusCode The status code
+// *@apiError (Error 404) {String} error The server can not find the requested resource. 
+// *@apiError (Error 404) {Number} statusCode The status code
+
+
+
+// * @apiError (Error 429) {String} error Too Many Requests ; the user has sent too many requests in a given amount of time ("rate limiting").
+// * @apiError (Error 429) {Number} statusCode The status code
+
+// *@apiErrorExample {json} Error-404
+// *     HTTP/1.1 404 Not Found
+// *     {
+// *       "error": "photoNotFound",
+// *        "statusCode":404
+// *     }
+// * 
+// @apiErrorExample {json} Error-401
+// *     HTTP/1.1 401 Unauthorized
+// *     {
+// *       "error": "Unauthorized User",
+// *        "statusCode":401
+// *     }
