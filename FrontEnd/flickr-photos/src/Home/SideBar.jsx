3d69749d301e36451de0085560e5f4085517e35f
AhmedMAyoub <ahmed2000mostafa@gmail.com>
2021-05-05 23:38:30 +0200

    Added Upgrade to Pro ad element only

--- FrontEnd/flickr-photos/src/Home/SideBar.jsx
+++ FrontEnd/flickr-photos/src/Home/SideBar.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './SideBar.css';
 import ExploreCard from './ExploreCard';
 import GroupsForYou from './GroupsForYou';
+import UpgradeToPro from './UpgradeToPro';
 
 const SideBar = () => (
   <div className="sideBarMain">
     <ExploreCard />
     <GroupsForYou />
-    <div className="upgradeToProCard" />
+    <UpgradeToPro />
   </div>
 );
 
---
+++ FrontEnd/flickr-photos/src/Home/UpgradeToPro.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './SideBar.css';
+
+const UpgradeToPro = () => (
+  <div />
+);
+
+export default UpgradeToPro;
