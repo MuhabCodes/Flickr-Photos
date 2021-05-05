641e837d38f4f3ed3a1e02a63d962bc49d196bf3
manaarr <manaarrzakaria@gmail.com>
2021-05-06 00:30:02 +0200

    check active url and style

--- FrontEnd/flickr-photos/src/Profile/SubNavBar.jsx
+++ FrontEnd/flickr-photos/src/Profile/SubNavBar.jsx
@@ -2,28 +2,36 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import MenuItems from './MenuItems';
 
-const SubNavBar = () => (
-  <div className="navbar">
-    <div className="fluid-sub-nav">
-      <div className="sub-nav-menu" role="navigation">
-        <div className="sub-nav-content">
-          <div className="sub-nav-container">
-            <ul
-              className="sub-nav-links"
-              role="menubar"
-              id="sub_nav_user_const"
-            >
-              {MenuItems.map((item) => (
-                <li key={item.key}>
-                  <Link to={item.url} className={item.cName}>{item.title}</Link>
-                </li>
-              ))}
-            </ul>
+const SubNavBar = () => {
+  const currentPath = window.location.pathname;
+  return (
+    <div className="navbar">
+      <div className="fluid-sub-nav">
+        <div className="sub-nav-menu" role="navigation">
+          <div className="sub-nav-content">
+            <div className="sub-nav-container">
+              <ul
+                className="sub-nav-links"
+                role="menubar"
+                id="sub_nav_user_const"
+              >
+                {MenuItems.map((item) => {
+                  let idval = 'notactive';
+                  if (currentPath === item.url) {
+                    idval = 'activeurl';
+                  } return (
+                    <li key={item.key}>
+                      <Link to={item.url} className={item.cName} id={idval}>{item.title}</Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default SubNavBar;
