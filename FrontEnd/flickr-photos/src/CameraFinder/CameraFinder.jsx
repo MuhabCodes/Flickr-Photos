725cf54af54f6284b30cd8d4c3f4e5beb6934445
YomnaTarek <yomnaragab13@gmail.com>
2021-05-04 21:53:04 -0700

    Graphs.jsx and its tag has been included in the CameraFinder.jsx

--- FrontEnd/flickr-photos/src/CameraFinder/CameraFinder.jsx
+++ FrontEnd/flickr-photos/src/CameraFinder/CameraFinder.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import PopCameras from './PopCameras';
 import RankTable from './RankTable';
+import Graphs from './Graphs';
 // This .jsx will include the components that will make up the Camera Finder webpage
 // The following function includes:
 // 1- useState that will help us set our data fetched
@@ -8,6 +9,7 @@ import RankTable from './RankTable';
 // 3- returns the title and subtitles of the webpage
 // 4- returns the components that will make up the page:
 // * The Most Popular Brands section which includes images, brand names and brand models
+// * Graphs have been added
 // * The Rank Table which includes all the brands in the .json and displays details such as
 // Rank, Brand, Top Models, Model Types, and # of Models.
 
@@ -32,7 +34,7 @@ const CameraFinder = () => {
         Most Popular Brands
       </h3>
       {cameras && <PopCameras cameras={cameras} />}
-
+      <Graphs />
       <h3 id="subtitleForTable">
         Camera Brands used in the Flickr Community
       </h3>
