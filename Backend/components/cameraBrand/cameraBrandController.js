77ce3b8f9b630b4a0404a4a195017e40f471dec2
yousefQadry <58371826+yousefQadry@users.noreply.github.com>
2021-05-05 21:44:07 +0200

    Added comments to all controller functions

--- Backend/components/cameraBrand/cameraBrandController.js
+++ Backend/components/cameraBrand/cameraBrandController.js
@@ -3,15 +3,16 @@ const cameraBrandDAL = require('./cameraBrandDAL');
 module.exports.createBrand = async function createBrand(req, res) {
   const { body } = req;
   try {
+    // checking if there is  a brand already named like this
     const brandObj = await cameraBrandDAL.createBrand(body.name);
     if (!brandObj) {
       return res.status(404).json({
         message: `There is already a brand named like this ${body.name}`,
       });
     }
-    return res.status(200).json(brandObj);
+    return res.status(200).json(brandObj); //  else create the brand
   } catch (err) {
-    return res.status(500).json({
+    return res.status(500).json({ // no connection to db
       error: err,
     });
   }
@@ -20,10 +21,15 @@ module.exports.createBrand = async function createBrand(req, res) {
 module.exports.getBrands = async function getBrands(req, res) {
   try {
     const brandObj = await cameraBrandDAL.getAllBrands();
-    return res.status(200).json({
+    if (brandObj.length === 0) { // if no brands exists in db
+      return res.status(404).json({
+        message: 'could not find any brands',
+      });
+    }
+    return res.status(200).json({ // else return the brands
       brands: [brandObj],
     });
-  } catch (err) {
+  } catch (err) { // couldn't connect to db
     return res.status(500).json({
       error: err,
     });
@@ -32,8 +38,13 @@ module.exports.getBrands = async function getBrands(req, res) {
 module.exports.getBrandWithName = async function getBrandWithName(req, res) {
   try {
     const brandObj = await cameraBrandDAL.getBrandWithName(req.params.brandName);
-    return res.status(200).json(brandObj);
+    if (brandObj.length === 0) { // if no brands exists in db
+      return res.status(404).json({
+        message: 'could not find any brands',
+      });
+    }
+    return res.status(200).json(brandObj); // else return the brands
   } catch (error) {
-    return res.status(404).json(error);
+    return res.status(500).json(error); // couldn't connect to db
   }
 };
