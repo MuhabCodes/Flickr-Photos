40a78c39aea128d92c0e13f9880fa8a3b26dc272
manaarr <manaarrzakaria@gmail.com>
2021-05-05 21:10:17 +0200

    added empty testimonials

---
+++ FrontEnd/flickr-photos/src/Profile/Testimonials.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TestimonialsArea = () => (
+  <div className="testimonials-view">
+    <h4>Testimonials</h4>
+    <div className="test-filter-bar" />
+    <div className="testimonials-container">
+      <div className="testimonials-list">
+        <p>Nobody has added a testimonial for you yet.</p>
+      </div>
+    </div>
+  </div>
+);
+
+export default TestimonialsArea;
