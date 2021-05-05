b564feb3a739b7c82a1ac57d148a007b741c6fdd
AhmedMAyoub <ahmed2000mostafa@gmail.com>
2021-05-05 23:37:59 +0200

    Changed some css for better responsivness

--- FrontEnd/flickr-photos/src/Home/HomePage.jsx
+++ FrontEnd/flickr-photos/src/Home/HomePage.jsx
@@ -7,50 +7,49 @@ const HomePage = () => {
   const data = 1;
   console.log(data);
   return (
-    <div className="homePageContentMain">
-      <div className="homePageFeed">
-        <div className="homeFeedFilters">
-          <div className="homeFeedDropDown">
-            ad
+    <div className="homePage">
+      <div className="homePageContentMain">
+        <div className="homePageFeed">
+          <div className="homeFeedFilters">
+            <div className="homeFeedDropDown">
+              ad
+            </div>
+            <div className="homeFeedLayouts">
+              ad
+            </div>
           </div>
-          <div className="homeFeedLayouts">
-            ad
+          <div className="homeFeedColumnContent">
+            <div className="noFollowingErrorDiv">
+              <div className="errorMessagediv">
+                <h2 className="errorFollowingTitle">There are no posts from the people you follow right now.</h2>
+                <p className="errorFollowingMessage">
+                  Check out the recommended photographers below and start following people
+                  to see their content here.
+                </p>
+              </div>
+            </div>
+            <div className="feed-people-to-follow">
+              <p>People to follow</p>
+              <div className="cards-container">
+                <div className="single-card">
+                  <PeopleCard />
+                </div>
+                <div className="single-card">
+                  <PeopleCard />
+                </div>
+                <div className="single-card">
+                  <PeopleCard />
+                </div>
+                <div className="single-card">
+                  <PeopleCard />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="homeFeedColumnContent">
-          <div className="noFollowingErrorDiv">
-            <div className="errorMessagediv">
-              <h2 className="errorFollowingTitle">There are no posts from the people you follow right now.</h2>
-              <p className="errorFollowingMessage">
-                Check out the recommended photographers below and start following people
-                to see their content here.
-              </p>
-            </div>
-          </div>
-          <div className="feed-people-to-follow">
-            <p>People to follow</p>
-            <div className="cards-container">
-              <div className="single-card">
-                <PeopleCard />
-              </div>
-              <div className="single-card">
-                <PeopleCard />
-              </div>
-              <div className="single-card">
-                <PeopleCard />
-              </div>
-              <div className="single-card">
-                <PeopleCard />
-              </div>
-            </div>
-            <div className="view-more">
-              View More
-            </div>
-          </div>
+        <div className="homePageSideBar">
+          <SideBar />
         </div>
-      </div>
-      <div className="homePageSideBar">
-        <SideBar />
       </div>
     </div>
   );
