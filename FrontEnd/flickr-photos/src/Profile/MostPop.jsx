7b6a2b9643601bcdbd14fef77037ed8990fea84d
manaarr <manaarrzakaria@gmail.com>
2021-05-05 21:09:22 +0200

    created most popular area with css

--- FrontEnd/flickr-photos/src/Profile/MostPop.jsx
+++ FrontEnd/flickr-photos/src/Profile/MostPop.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import { Link } from 'react-router-dom';
 import images from './imagesArray';
 
 const MostPop = () => (
@@ -6,11 +7,21 @@ const MostPop = () => (
     <div className="pop-container">
       <h4>Most popular photos</h4>
       <div className="dropdown" />
-      <div className="grid">
+      <div className="grid" id="pop-exist">
         {images.map((image) => (
           <img className="grid-item" src={image.src} alt="" />
         ))}
       </div>
+      {/* <div className="empty-pop" id="pop-false">
+        <h3>Your most popular photos will appear here.</h3>
+        <p>Set your photos to public for more faves, comments and views.</p>
+        <p>For even ore exposure, add them to Groups</p>
+        <p>
+          <Link to="/cameraroll">
+            Go to Camera Roll
+          </Link>
+        </p>
+        </div> */}
     </div>
   </div>
 );
