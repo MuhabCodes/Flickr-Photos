c774ec3de31e651f966cae4d59f917f2977a9565
YomnaTarek <yomnaragab13@gmail.com>
2021-05-04 20:09:25 -0700

    Title and subtitle for the first section of the camera finder page has been added

---
+++ FrontEnd/flickr-photos/src/CameraFinder/CameraFinder.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CameraFinder = () => (
+  <div className="cameraFinder">
+    <h1 id="title">
+      Camera Finder
+    </h1>
+    <h3 id="subtitle">
+      Most Popular Brands
+    </h3>
+  </div>
+);
+export default CameraFinder;
