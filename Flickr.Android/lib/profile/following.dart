644e783ef723d872d3ea80be0f1c9167eb66c38a
Mustafa Usama <mustafausamaa@gmail.com>
2021-05-06 06:51:21 +0200

    Added following page

---
+++ Flickr.Android/lib/profile/following.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Following extends StatefulWidget {
+  @override
+  _FollowingState createState() => _FollowingState();
+}
+
+class _FollowingState extends State<Following> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey[800],
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Following"),
+          ],
+        ),
+      ),
+      body: Card(
+        elevation: 5,
+      ),
+    );
+  }
+}
