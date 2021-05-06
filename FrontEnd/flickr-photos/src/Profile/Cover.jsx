77d447d7f54e7ac7c3627b29859e23027f04dfb7
manaarr <manaarrzakaria@gmail.com>
2021-05-06 16:54:53 +0200

    added proBadge and fetch data

--- FrontEnd/flickr-photos/src/Profile/Cover.jsx
+++ FrontEnd/flickr-photos/src/Profile/Cover.jsx
@@ -1,44 +1,88 @@
 import React from 'react';
-import background from './assets/shapes_heart.png';
-import avatar from './assets/logo192.png';
+// import { useParams } from 'react-router';
+import useFetch from './usefetch';
+// import background from './assets/shapes_heart.png';
+// import avatar from './assets/logo192.png';
 
-const CoverArea = () => (
-  <div className="cover">
-    <div
-      className="changing-cover-photo-bg"
-      style={{ backgroundImage: `url(${background})` }}
-    >
-      <div className="cover-photo-gradient" />
-      <div className="cover-content">
-        <button type="button" className="cover-photo-editbutton">
-          <img src="./assets/edit_icon.png" alt="" />
-          edit
-        </button>
+const CoverArea = () => {
+  // const { id } = useParams();
+  const { data: Coverinfo } = useFetch('http://localhost:8000/CoverInfo/85@N00');
+  const checkBg = () => {
+    if (Coverinfo.coverUrl === '0') {
+      console.log('happens');
+      Coverinfo.coverUrl = 'https://media.istockphoto.com/photos/white-studio-background-picture-id1040250650?k=6&m=1040250650&s=612x612&w=0&h=Ve0znmMwCbVyo66uIfeSrSYRuHau85oBiVIv1OplATs=';
+    }
+    if (Coverinfo.avatarUrl === '0') {
+      Coverinfo.avatarUrl = 'https://i.pinimg.com/originals/5c/dc/51/5cdc51216b63896814919fe5382bf752.jpg';
+    }
+    if (Coverinfo.isPro === '0') {
+      document.getElementById('pro-badge-cover').style.display = 'none';
+    } else {
+      document.getElementById('pro-badge-cover').style.display = 'flex';
+    }
+  };
+  return (
+    <div className="cover">
+      {Coverinfo && checkBg()}
+      {Coverinfo && (
         <div
-          className="user-cover-avatar"
-          style={{ backgroundImage: `url(${avatar})` }}
-        />
-        <div className="title-content">
-          <div className="user-cover-title">
-            <h1 className="user-cover-name">Manar Nassef</h1>
-          </div>
-          <div className="follow-view">
-            <p className="user-cover-display-name">manarr__</p>
-            <p className="following-followers-cover-list">
-              989 Followers
-              {' '}
-              <em>•</em>
-              {' '}
-              1000 Following
-            </p>
-            <p className="cover-spacer" />
-            <p className="user-cover-photo-count">50 photos</p>
-            <p className="user-cover-join-date">joined 2021</p>
+          className="changing-cover-photo-bg"
+          style={{ backgroundImage: `url(${Coverinfo.coverUrl})` }}
+        >
+          <div className="cover-photo-gradient" />
+          <div className="cover-content">
+            <button type="button" className="cover-photo-editbutton">
+              <img src="./assets/edit_icon.png" alt="" />
+              edit
+            </button>
+
+            <div
+              className="user-cover-avatar"
+              style={{ backgroundImage: `url(${Coverinfo.avatarUrl})` }}
+            />
+            <div className="title-content">
+              <div className="user-cover-title">
+                <h1 className="user-cover-name">
+                  {Coverinfo.firstName}
+                  {' '}
+                  {Coverinfo.lastName}
+                </h1>
+              </div>
+              <div className="follow-view">
+                <a href="/account/upgrade/po" id="pro-badge-cover">
+                  <p className="pro-badge-style">Pro</p>
+                </a>
+                <p className="user-cover-display-name">
+                  {Coverinfo.displayName}
+                </p>
+                <p className="following-followers-cover-list">
+                  {Coverinfo.Followers}
+                  {' '}
+                  Followers
+                  {' '}
+                  <em>•</em>
+                  {' '}
+                  {Coverinfo.Following}
+                  {' '}
+                  Following
+                </p>
+                <p className="cover-spacer" />
+                <p className="user-cover-photo-count">
+                  {Coverinfo.Photos}
+                  {' '}
+                  photos
+                </p>
+                <p className="user-cover-join-date">
+                  joined
+                  {' '}
+                  {Coverinfo.Joined}
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
-  </div>
-);
-
+  );
+};
 export default CoverArea;
