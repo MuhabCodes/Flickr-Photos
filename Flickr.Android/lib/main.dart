d2cd5a258c97bde141d2db2d1833d96c1b172aea
Mustafa Usama <mustafausamaa@gmail.com>
2021-05-06 06:53:36 +0200

    Added AboutProvider for main

--- Flickr.Android/lib/main.dart
+++ Flickr.Android/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flickr/login/get_started.dart';
+import 'package:flickr/navigations/top_nav_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter/services.dart';
+import 'providers/about_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,9 +15,12 @@ class MyApp extends StatelessWidget {
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
     ]);
-    return MaterialApp(
-      home: GetStarted(),
-      debugShowCheckedModeBanner: false, //remove debug sign
+    return ChangeNotifierProvider.value(
+      value: AboutProvider(),
+      child: MaterialApp(
+        home: GetStarted(),
+        debugShowCheckedModeBanner: false, //remove debug sign
+      ),
     );
   }
 }
