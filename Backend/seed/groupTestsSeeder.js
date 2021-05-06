6c14afbd67907834d31aa160319eda75f010017f
ahmedehabb <Ahmed.bahloul00@eng-st.cu.edu.eg>
2021-05-07 00:45:59 +0200

    added requirement(dotenv) to seeder files

--- Backend/seed/groupTestsSeeder.js
+++ Backend/seed/groupTestsSeeder.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const { join } = require('path');
 const Group = require('../components/Group/groupModel');
+require('dotenv').config({ path: join(__dirname, '/../secret/', '.env') });
 
 const group = new Group({
   _id: mongoose.Types.ObjectId('111111111111111111111111'),
