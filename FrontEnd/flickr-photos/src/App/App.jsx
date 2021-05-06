8c8ff52c6f21a7c1ff7872e1b987e6717bcd8a2f
SandySamir236 <63124393+SandySamir236@users.noreply.github.com>
2021-05-06 20:13:25 +0200

    navbar and footer final

--- FrontEnd/flickr-photos/src/App/App.jsx
+++ FrontEnd/flickr-photos/src/App/App.jsx
@@ -9,22 +9,6 @@ function App() {
     <div>
       <NavBar />
       <Footer />
-      <h1>sandy</h1>
-      <h1>sandy</h1>
-      <h1>sandy</h1>
-      <h1>sandy</h1>
-      <h1>sandy</h1>
-      <h1>sandy</h1>
-      <h1>sandy</h1>
-      <h1>sandy</h1>
-      <h1>sandy</h1>
-      <h1>sandy</h1>
-      <h1>sandy</h1>
-      <h1>sandy</h1>
-      <h1>sandy</h1>
-      <h1>sandy</h1>
-      <h1>sandy</h1>
-      <h1>sandy</h1>
 
     </div>
   );
