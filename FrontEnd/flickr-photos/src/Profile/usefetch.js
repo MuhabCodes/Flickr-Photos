fea1a83e21b74f3269808155a3e2ca514c09f4c4
manaarr <manaarrzakaria@gmail.com>
2021-05-06 16:55:17 +0200

    useFetch hook

--- FrontEnd/flickr-photos/src/Profile/usefetch.js
+++ FrontEnd/flickr-photos/src/Profile/usefetch.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const useFetch = (url) => {
   const [data, setData] = useState(null);
@@ -7,26 +7,35 @@ const useFetch = (url) => {
 
   useEffect(() => {
     const abortCont = new AbortController();
-    fetch(url, { signal: abortCont.signal })
-      .then((res) => {
-        if (!res.ok) {
-          throw Error('could not fetch data');
-        }
-        return res.json();
-      })
-      .then((da) => {
-        setData(da);
-        setIsPending(false);
-        setError(null);
-      })
-      .catch((err) => {
-        if (err.name !== 'AbortError') {
-          setError(err.message);
+
+    setTimeout(() => {
+      fetch(url, { signal: abortCont.signal })
+        .then((res) => {
+          if (!res.ok) { // error coming back from server
+            throw Error('could not fetch the data for that resource');
+          }
+          return res.json();
+        })
+        .then((Data) => {
           setIsPending(false);
-        }
-      });
+          setData(Data);
+          setError(null);
+        })
+        .catch((err) => {
+          if (err.name === 'AbortError') {
+            console.log('fetch aborted');
+          } else {
+          // auto catches network / connection error
+            setIsPending(false);
+            setError(err.message);
+          }
+        });
+    });
+
+    // abort the fetch
     return () => abortCont.abort();
   }, [url]);
+
   return { data, isPending, error };
 };
 
