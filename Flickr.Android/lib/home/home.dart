839d017728f43beb03894509daaf4466113cf5b6
mehrez1110 <mohamed.ali00@eng-st.cu.edu.eg>
2021-05-06 04:16:10 +0200

    Added Filler pages for Top Navigation Bar

---
+++ Flickr.Android/lib/home/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+          'home'), //tester to home page which prints home at the center of the page
+    );
+  }
+}
