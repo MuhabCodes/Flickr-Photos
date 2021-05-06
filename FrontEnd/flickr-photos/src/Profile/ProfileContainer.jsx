c1c44252353db2bdf4db72308a919b51b66a47db
manaarr <manaarrzakaria@gmail.com>
2021-05-07 00:18:13 +0200

    profile routing

--- FrontEnd/flickr-photos/src/Profile/ProfileContainer.jsx
+++ FrontEnd/flickr-photos/src/Profile/ProfileContainer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import SubNavBar from './SubNavBar';
 import AboutBio from './AboutBio';
 import Stats from './GeneralStats';
 import Showcase from './Showcase';
@@ -9,7 +10,6 @@ import TestimonialsArea from './Testimonials';
 // const { data, isPending, Error } = useFetch('http://localhost:8000/aboutbio');
 
 const ProfileContainer = () => (
-
   <div className="fluid-profile-container">
     <div className="profile-bio-page">
       <div className="bio-description-section">
