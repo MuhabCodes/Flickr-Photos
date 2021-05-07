6090537446f7370eff9a002e953b1efbedb095bd
IShamss <mohamedshams2000@gmail.com>
2021-05-07 13:53:06 +0200

    reformat the date

--- docs/photos/addPhoto.js
+++ docs/photos/addPhoto.js
@@ -24,19 +24,19 @@
  *
  *
  * @apiParamExample  {json} Request-Example:
- * {
+ * {[{
  *      "authorId" : "nhuefbh432",
  *      "title":"Blue Lake",
  *      "description":"A fantastic view on the lake",
- *      "captureDate":"1-1-2021",
- *      "uploadDate":"3-1-2021",
+ *      "captureDate":"2020-05-01T01:50:08",
+ *      "uploadDate":"2021-12-01T19:11:08",
  *      "isPublic":true,
  *      "secret":"dfabhfu234",
  *      "favorites":false,
  *      "inPhoto":[{...},{...},...],
  *      "tags":[{...},{...},...],
  *      "cameraName":"Cannon 201cf"
- * }
+ *  },{...},...] }
  *
  *
  * @apiSuccessExample {json} Success-Response:
