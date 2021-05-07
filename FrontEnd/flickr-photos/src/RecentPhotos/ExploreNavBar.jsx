af75fb813c1b75b712ebd367abf6c3b027a44bb1
YomnaTarek <yomnaragab13@gmail.com>
2021-05-07 01:02:25 -0700

    Link has been added instead of a tags

--- FrontEnd/flickr-photos/src/RecentPhotos/ExploreNavBar.jsx
+++ FrontEnd/flickr-photos/src/RecentPhotos/ExploreNavBar.jsx
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import React from 'react';
 import 'bootstrap';
-
+import { Link } from 'react-router-dom';
 // The ExploreNavbar.jsx will include a function 'ExploreNavbar' that returns the html tags and
 // properties to make up this navbar.
 // The following 2 lines are jquery code to toggle the dropdown list 'More' on click
@@ -12,20 +12,20 @@ $(document).ready(() => {
 function ExploreNavBar() {
   return (
     <nav className="expnav">
-      <a id="menu-item" href="./Explore" className="active">Explore</a>
-      <a id="menu-item" href="./Trending">Trending</a>
-      <a id="menu-item" href="./Events">Events</a>
+      <Link id="menu-item" to="./" className="active">Explore</Link>
+      <Link id="menu-item" to="./Trending">Trending</Link>
+      <Link id="menu-item" to="./Events">Events</Link>
       <div className="animation start-home" />
       <div className="dropdown">
         <button className="dropdown-toggle" type="button" id="dropdownMenuButton1" data-toggle="dropdown" aria-expanded="false">
           More
         </button>
         <div className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-          <a className="dropdown-item" href="./TheCommons">The Commons</a>
-          <a className="dropdown-item" href="./Galleries">Galleries</a>
-          <a className="dropdown-item" href="./WorldMap">World Map</a>
-          <a className="dropdown-item" href="./CameraFinder">Camera Finder</a>
-          <a className="dropdown-item" href="./FlickrBlog">Flickr Blog</a>
+          <Link className="dropdown-item" to="./TheCommons">The Commons</Link>
+          <Link className="dropdown-item" to="./Galleries">Galleries</Link>
+          <Link className="dropdown-item" to="./WorldMap">World Map</Link>
+          <Link className="dropdown-item" to="./CameraFinder">Camera Finder</Link>
+          <Link className="dropdown-item" to="./FlickrBlog">Flickr Blog</Link>
         </div>
       </div>
     </nav>
