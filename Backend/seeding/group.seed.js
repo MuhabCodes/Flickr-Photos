3228610ed1f7affe87e729f5816f8ea1e410917d
Abdelrhman-Hosny <ahosny2792@gmail.com>
2021-05-07 06:08:40 +0200

    add ownerId to group seeding

--- Backend/seeding/group.seed.js
+++ Backend/seeding/group.seed.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const { join } = require('path');
-const Group = require('../components/Group/groupModel');
+const Group = require('../components/group/groupModel');
 require('dotenv').config({ path: join(__dirname, '/../secret/', '.env') });
 
 const group = new Group({
   _id: mongoose.Types.ObjectId('111111111111111111111111'),
   name: 'testing',
+  ownerId: mongoose.Types.ObjectId('111111111111111111111112'),
 });
 
 async function groupSeed() {
