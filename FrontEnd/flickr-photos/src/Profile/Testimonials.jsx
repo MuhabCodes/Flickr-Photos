eb339d3ca0b8ac49a851c21afd43e3400ec15af8
AhmedMAyoub <ahmed2000mostafa@gmail.com>
2021-05-07 20:50:06 +0200

    Manar Styles Done

--- FrontEnd/flickr-photos/src/Profile/Testimonials.jsx
+++ FrontEnd/flickr-photos/src/Profile/Testimonials.jsx
@@ -3,11 +3,11 @@ import './Testimonials.css';
 // import pic from './assets/logo192.png';
 
 const TestimonialsArea = () => (
-  <div className="testimonials-view">
+  <div className="testimonials-view-tP">
     <h4>Testimonials</h4>
-    <div className="test-filter-bar" />
-    <div className="testimonials-container">
-      <div className="testimonials-list">
+    <div className="test-filter-bar-tP" />
+    <div className="testimonials-container-tP">
+      <div className="testimonials-list-tP">
         <p id="empty-testimmonials">Nobody has added a testimonial for you yet.</p>
       </div>
     </div>
--- FrontEnd/flickr-photos/src/Profile/UserInfo.jsx
+++ FrontEnd/flickr-photos/src/Profile/UserInfo.jsx
@@ -6,47 +6,51 @@ import './UserInfo.css';
 const UserInfo = () => {
   // const [isDrop, setisDrop] = useState(false);
   function Edit() {
-    document.getElementById('actual-info').style.display = 'none';
-    document.getElementById('edit-form-container').style.display = 'flex';
+    document.getElementById('actual-info-uiP').style.display = 'none';
+    document.getElementById('edit-form-container-uiP').style.display = 'flex';
   }
   function toggleMenu() {
     document.getElementById('dropdownmenu-ui').style.display = 'flex';
   }
+  function Cancel() {
+    document.getElementById('actual-info-uiP').style.display = 'flex';
+    document.getElementById('edit-form-container-uiP').style.display = 'none';
+  }
   return (
-    <div className="user-info">
-      <div className="user-info-container">
-        <div className="actions-container">
+    <div className="user-info-uiP">
+      <div className="user-info-container-uiP">
+        <div className="actions-container-userinfoP">
           <button type="button" className="edit-button" onClick={Edit}>
             <img src={edit} alt="" className="edit-button-img" />
           </button>
         </div>
-        <div id="edit-form-container" style={{ display: 'none' }}>
-          <ul>
-            <li>
-              <span className="title">Joined</span>
-              <span className="user-data">April 2021</span>
+        <div id="edit-form-container-uiP" style={{ display: 'none' }}>
+          <ul className="userinfo-ul-style">
+            <li className="userinfo-list-style">
+              <span className="title-uiP">Joined</span>
+              <span className="user-data-uiP">April 2021</span>
             </li>
-            <li>
+            <li className="userinfo-list-style">
               <label htmlFor="inputOccupation">
-                <span className="title">Occupation</span>
-                <input type="text" id="inputOccupation" />
+                <span className="title-uiP">Occupation</span>
+                <input className="input-uiP" type="text" id="inputOccupation" />
               </label>
             </li>
-            <li>
+            <li className="userinfo-list-style">
               <label htmlFor="inputHometown">
-                <span className="title">Hometown</span>
-                <input type="text" id="inputHometown" />
+                <span className="title-uiP">Hometown</span>
+                <input className="input-uiP" type="text" id="inputHometown" />
               </label>
             </li>
-            <li>
+            <li className="userinfo-list-style">
               <label htmlFor="inputCurrentCity">
-                <span className="title">Current city</span>
-                <input type="text" id="inputCurrentCity" />
+                <span className="title-uiP">Current city</span>
+                <input className="input-uiP" type="text" id="inputCurrentCity" />
               </label>
-              <span className="filter-img">
-                <img src={filter} alt="" className="privacy" />
+              <span className="filter-img-uiP">
+                <img src={filter} alt="" className="privacy-uiP" />
                 <span className="dropdown">
-                  <button className="btn btn-secondary btn-sm dropdown-toggle" type="button" id="dropdownMenuButton" aria-label="dropdown-ui" />
+                  <button className="btn btn-secondary btn-sm dropdown-toggle" type="button" id="dropdownMenuButton-uiP" aria-label="dropdown-ui" />
                   <div className="dropdown-menu" id="dropdownmenu-ui" aria-labelledby="dropdowbMenuButton">
                     <a href="/" className="dropdown-menu">Everone</a>
                     <a href="/" className="dropdown-menu">Any Flickr Member</a>
@@ -56,19 +60,19 @@ const UserInfo = () => {
                 </span>
               </span>
             </li>
-            <li>
+            <li className="userinfo-list-style">
               <label htmlFor="inputCountry">
-                <span className="title">Country</span>
-                <input type="text" id="inputCountry" />
+                <span className="title-uiP">Country</span>
+                <input className="input-uiP" type="text" id="inputCountry" />
               </label>
             </li>
-            <li>
-              <span className="title">Email</span>
-              <span className="user-data">manaarrzakaria@gmail.com</span>
-              <span className="filter-img">
-                <img src={filter} alt="" className="privacy" />
+            <li className="userinfo-list-style">
+              <span className="title-uiP">Email</span>
+              <span className="user-data-uiP">manaarrzakaria@gmail.com</span>
+              <span className="filter-img-uiP">
+                <img src={filter} alt="" className="privacy-uiP" />
                 <span className="dropdown">
-                  <button className="btn btn-secondary btn-sm dropdown-toggle" type="button" id="dropdownMenuButton" aria-label="dropdown-ui" onClick={toggleMenu} />
+                  <button className="btn btn-secondary btn-sm dropdown-toggle" type="button" id="dropdownMenuButton-uiP" aria-label="dropdown-ui" onClick={toggleMenu} />
                   <div className="dropdown-menu" id="dropdownmenu-ui" aria-labelledby="dropdowbMenuButton">
                     <a href="/" className="dropdown-menu">Everone</a>
                     <a href="/" className="dropdown-menu">Any Flickr Member</a>
@@ -79,60 +83,60 @@ const UserInfo = () => {
               </span>
             </li>
           </ul>
-          <ul>
-            <li>
+          <ul className="userinfo-ul-style">
+            <li className="userinfo-list-style">
               <label htmlFor="inputWebsite">
-                <span className="title">Website</span>
-                <input type="text" id="inputWebsite" />
+                <span className="title-uiP">Website</span>
+                <input className="input-uiP" type="text" id="inputWebsite" />
               </label>
             </li>
-            <li>
+            <li className="userinfo-list-style">
               <label htmlFor="inputFacebook">
-                <span className="title">Facebook</span>
-                <input type="text" id="inputFacebook" placeholder="your username or profile link" />
+                <span className="title-uiP">Facebook</span>
+                <input className="input-uiP" type="text" id="inputFacebook" placeholder="your username or profile link" />
               </label>
             </li>
-            <li>
+            <li className="userinfo-list-style">
               <label htmlFor="inputTwitter">
-                <span className="title">Twitter</span>
-                <input type="text" id="inputTwitter" placeholder="your username or profile link" />
+                <span className="title-uiP">Twitter</span>
+                <input className="input-uiP" type="text" id="inputTwitter" placeholder="your username or profile link" />
               </label>
             </li>
-            <li>
+            <li className="userinfo-list-style">
               <label htmlFor="inputInstagram">
-                <span className="title">Instagram</span>
-                <input type="text" id="inputInstagram" placeholder="your username or profile link" />
+                <span className="title-uiP">Instagram</span>
+                <input className="input-uiP" type="text" id="inputInstagram" placeholder="your username or profile link" />
               </label>
             </li>
-            <li>
+            <li className="userinfo-list-style">
               <label htmlFor="inputPinteret">
-                <span className="title">Pinterest</span>
-                <input type="text" id="inputPinterest" placeholder="your username or profile link" />
+                <span className="title-uiP">Pinterest</span>
+                <input className="input-uiP" type="text" id="inputPinterest" placeholder="your username or profile link" />
               </label>
             </li>
-            <li>
+            <li className="userinfo-list-style">
               <label htmlFor="inputTumblr">
-                <span className="title">Tumblr</span>
-                <input type="text" id="inputTumblr" placeholder="your username or profile link" />
+                <span className="title-uiP">Tumblr</span>
+                <input className="input-uiP" type="text" id="inputTumblr" placeholder="your username or profile link" />
               </label>
             </li>
             <li className="done-ui">
-              <span className="actions">
+              <span className="actions-userinfoP">
                 <button type="button" className="save-ui">Save</button>
-                <button type="button" className="cancel-ui">Cancel</button>
+                <button type="button" className="cancel-ui" onClick={Cancel}>Cancel</button>
               </span>
             </li>
           </ul>
         </div>
-        <div className="info-view-container" id="actual-info">
-          <ul>
-            <li>
-              <span className="title">Joined</span>
-              <span className="user-data"> April 2021</span>
+        <div className="info-view-container-uiP" id="actual-info-uiP">
+          <ul className="userinfo-ul-style">
+            <li className="userinfo-list-style">
+              <span className="title-uiP">Joined</span>
+              <span className="user-data-uiP"> April 2021</span>
             </li>
-            <li>
-              <span className="title">Email</span>
-              <span className="user-data">manarzakaria@gmail.com</span>
+            <li className="userinfo-list-style">
+              <span className="title-uiP">Email</span>
+              <span className="user-data-uiP">manarzakaria@gmail.com</span>
             </li>
           </ul>
           <ul className="spacer" />
