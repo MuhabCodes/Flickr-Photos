72f07b476d85f2b603623da625854251eef34066
George-Joseph99 <george_joseph99@hotmail.com>
2021-05-08 06:47:54 +0200

    Create info.py
    Contains all used links

---
+++ Testing/Web/info/info.py
@@ -0,0 +1,17 @@
+# Contains URLs needed for testing
+
+class TestData(object):
+    MAIN_URL = "https://www.flickr.com/"
+    LOGIN_URL = "https://identity.flickr.com/login"
+    SIGNUP_URL = "https://identity.flickr.com/sign-up"
+    HELP_URL = "https://help.flickr.com/"
+    PRIVACY_URL = "https://www.flickr.com/help/privacy"
+    TERMS_URL = "https://www.flickr.com/help/terms"
+    YAHOO_REDIRECT_URL = "https://login.yahoo.com/"
+    ABOUT_URL = "https://www.flickr.com/about"
+    JOBS_URL = "https://www.flickr.com/jobs"
+    BLOG_URL = "https://blog.flickr.net/en"
+    DEVELOPERS_URL = "https://www.flickr.com/services/developer"
+    GUIDELINES_URL = "https://www.flickr.com/help/guidelines"
+    HELP_FORUM_URL = "https://www.flickr.com/help/forum/en-us/"
+    COOKIES_URL = "https://www.flickr.com/help/cookies"
