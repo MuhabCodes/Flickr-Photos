a4fc59c738f492b8f0841ac5c13a6cfe967a9949
AhmedM20 <55909284+AhmedM20@users.noreply.github.com>
2021-05-08 11:13:20 +0200

    Changed ClassNames & added linkss

--- FrontEnd/flickr-photos/src/App/GroupMembers/GroupNavBar.jsx
+++ FrontEnd/flickr-photos/src/App/GroupMembers/GroupNavBar.jsx
@@ -11,28 +11,28 @@ function GroupNavBar() {
           <div className="group-nav-items">
             <ul className="group-links" role="menubar">
               <li id="overview" className="group-link" role="menuitem">
-                <a href=".">
+                <a href="/groups/" className="group-nav-items-a">
                   <span>Overview</span>
                 </a>
               </li>
               <li id="discussions" className="group-link" role="menuitem">
-                <a href=".">
+                <a href="/groups/discussions/" className="group-nav-items-a">
                   <span>Discussions</span>
                 </a>
               </li>
               <li id="pool" className="group-link" role="menuitem">
-                <a href=".">
+                <a href="/groups/pool/" className="group-nav-items-a">
                   <span>Photos</span>
                 </a>
               </li>
               <li id="members" className="group-link group-selected" role="menuitem">
-                <a href="/members/">
+                <a href="/groups/members/" className="group-nav-items-a">
                   <span>Members</span>
 
                 </a>
               </li>
               <li id="map" className="group-link" role="menuitem">
-                <a href=".">
+                <a href="/groups/map/" className="group-nav-items-a">
                   <span>Map</span>
                 </a>
               </li>
