16bfaad2f1223671181ad3072b101b984fed23ca
YomnaTarek <yomnaragab13@gmail.com>
2021-05-16 21:40:16 -0700

    Adjusted classnames to obey the naming convention in Explore.jsx

--- FrontEnd/src/RecentPhotos/Explore.jsx
+++ FrontEnd/src/RecentPhotos/Explore.jsx
@@ -5,16 +5,16 @@ import RecentPhotos from './RecentPhotos';
 // The Explore.jsx will include the title of the webpage, nav bars as well as
 // the components of Recent Photos
 const Explore = () => (
-  <div className="recentPhotos">
+  <div className="recent-photos">
     <ExploreNavBar />
-    <div className="explorePageContent">
+    <div className="explore-page-content">
       <h1 id="exploreTitle">
-        <br className="breakLines" />
-        <br className="breakLines" />
-        <br className="breakLines" />
+        <br className="break-lines" />
+        <br className="break-lines" />
+        <br className="break-lines" />
         Explore
       </h1>
-      <br className="breakLines" />
+      <br className="break-lines" />
       <RecentPhotos />
     </div>
   </div>
