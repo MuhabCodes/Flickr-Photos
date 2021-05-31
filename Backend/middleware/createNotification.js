0dca5a116f7f3486acdc0aa4e6547ce0c1ee54e5
ahmedehabb <Ahmed.bahloul00@eng-st.cu.edu.eg>
2021-05-31 15:01:14 +0200

    added notification middleware

---
+++ Backend/middleware/createNotification.js
@@ -0,0 +1,39 @@
+const Notification = require('../components/notification/notificationModel');
+const { getPhotoById } = require('../components/photo/photoDAL');
+
+const createNotification = async (req, res) => {
+  const { photoId } = req.params;
+
+  try {
+    const photo = await getPhotoById(photoId, res);
+    let authorId;
+    if (photo) {
+      authorId = photo.authorId;
+    } else {
+      return res.status(404).json({
+        message: 'Photo Not Found',
+        favoriteCreated: req.favoriteCreated,
+      }); // this mean dont do anything if he doesnt find photo
+    }
+    const newNotification = new Notification({
+    // senderID: userId, // TODO
+      recieverID: authorId,
+      act: 'like',
+      photoId,
+      notificationDate: req.body.favoriteDate,
+    });
+    console.log(newNotification);
+    return res.status(201).json({
+      message: 'Favorite added succesfully',
+      favoriteCreated: req.favoriteCreated,
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: err,
+    });
+  }
+};
+
+module.exports = {
+  createNotification,
+};
