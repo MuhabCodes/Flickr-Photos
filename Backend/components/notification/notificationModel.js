26c3f4b6d85eec4076719e70d9caf0909cc7e25a
ahmedehabb <Ahmed.bahloul00@eng-st.cu.edu.eg>
2021-05-31 12:38:56 +0200

    added notification model

---
+++ Backend/components/notification/notificationModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const notificationSchema = mongoose.Schema({
+  senderID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  recieverID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  photoId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Photo',
+  },
+  act: {
+    type: String, // it will be one of three ("follow/like/comment")
+    required: true,
+  },
+  notificationDate: {
+    type: Date,
+  },
+
+});
+
+module.exports = mongoose.model('Notification', notificationSchema);
