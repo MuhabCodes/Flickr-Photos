dade1f6433be6eb1e61697dadf8c35449ad6b756
ahmedehabb <Ahmed.bahloul00@eng-st.cu.edu.eg>
2021-05-31 15:02:37 +0200

    factored router to handle middlewares

--- Backend/components/favorites/favoritesRouter.js
+++ Backend/components/favorites/favoritesRouter.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 const favoriteController = require('./favoritesController');
+const { createNotification } = require('../../middleware/createNotification');
 
 require('dotenv').config();
 
 router.post('/:photoId',
-  favoriteController.add);
+  favoriteController.add, createNotification);
 
 router.delete('/:photoId', favoriteController.deleteFavorite);
 
