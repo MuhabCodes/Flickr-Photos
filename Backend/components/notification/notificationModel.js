ef0fbb4ff6ccddab4bee1366e845307d18ef7742
Ahmed Ehab <Ahmed.bahloul00@eng-st.cu.edu.eg>
2021-06-01 15:25:12 +0200

    added sender id

--- Backend/components/notification/notificationModel.js
+++ Backend/components/notification/notificationModel.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const notificationSchema = mongoose.Schema({
-  // senderID: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: 'User',
-  //   required: true,
-  // }, // TODO WILL BE REMOVED
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  }, // TODO WILL BE REMOVED
   reciever: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
