06b885b5a78a9e218061bd560c87ee0b48c85511
manaarr <manaarrzakaria@gmail.com>
2021-06-01 21:15:04 +0200

    faves page

--- FrontEnd/src/Profile/faves.jsx
+++ FrontEnd/src/Profile/faves.jsx
@@ -1,30 +1,106 @@
-import React from 'react';
-import images from './imagesArray';
+import React, { useState, useEffect } from 'react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 import './faves.css';
+import configData from '../config.json';
+// The Faves.jsx is the component which helps in displaying the recent photos,
+// It includes the function 'Faves' which has a code the helps in fetching the photos from
+// a mock server. This function returns the fetched images, but not all at once,
+// but a couple at a time and the more the user scrolls down,
+// the more images are fetched and displayed.
+const Faves = () => {
+  const [photos, setRecPhotos] = useState([]);
+  const [toggled, setToggle] = useState(false);
+  // useEffect helps us fetch the photos from the mock server.
+  useEffect(() => {
+    fetch(`${configData.SERVER_URL}/photosExplore`)
+      .then((res) => res.json())
+      .then((data) => {
+        setRecPhotos(data);
+      })
+      .catch(() => {
+      });
+  }, []);
+  // const obj = JSON.parse(photos);
+  // console.log(obj.favs);
+  // The following changes the photos(objects) to array that can be used by the .map
+  // function in the return block.
+  const photoArr = Array.from(photos);
+  // console.log(photoArr[1].favs);
+  // function IncFave() {
+  //   if (toggled === true) {
+  //     const count = document.getElementById('favNum').innerText;
+  //     console.log(count);
+  //   }
+  // }
+  // The following function toggles the fave icon on click
+  function ClickMe(e) {
+    if (e.target.getAttribute('src') === 'https://img.icons8.com/android/24/ffffff/star.png') {
+      e.target.setAttribute('src', 'https://img.icons8.com/ios-filled/25/ffffff/star--v1.png');
+      // console.log(photoArr[].favs);
+      setToggle(!toggled);
+      // IncFave();
+      // let count = e.target.
+      // count += 1;
+      // console.log(count);
+    } else if (e.target.getAttribute('src') === 'https://img.icons8.com/ios-filled/25/ffffff/star--v1.png') {
+      e.target.setAttribute('src', 'https://img.icons8.com/android/24/ffffff/star.png');
+      setToggle(toggled);
+    }
+  }
 
-const Faves = () => (
-  <div className="faves-view-up">
-    <div className="faves-container-up">
-      {images.map((image) => (
-        <img className="grid-item-favesup" src={image.src} alt="" />
-        /* <div className="interaction-view">
-            <div className="photo-list-interaction">
-              <div className="interaction-bar">
-                <div className="text-faves">
-                  <a href={image.src} className="image-title-faves" tabIndex="-1">{image.title}</a>
-                  <a href="/photos" className="faves-attribution">
-                    by
-                    {' '}
-                    {image.user}
-                  </a>
-                </div>
-              </div>
-            </div>
+  // The following if condition checks if no images are in photos (fetched), display loading.
+  if (!photos) {
+    return <h1>No Photos Found</h1>;
+  }
+  // The following part is the one returns the fetched images.
+  // The lazy load Image component is used to display the images but not all at once,
+  // when the user scrolls down more images are fetched to be displayed.
+  // The next lines are to display the image description, username, faves and comments count
+  // when hovering on an image
+  return (
+    <div className="recent-photos">
+      <div className="image-grid">
+        {photoArr.map((photo) => (
+          <div className="image-container">
+            <LazyLoadImage
+              className="single-image"
+              src={photo.imagePath}
+              alt=""
+              key={photo.photoId}
+            />
+            <span className="text-area">
+              <span className="description">
+                {photo.description}
+              </span>
+              <span className="user-name-explore">
+                by
+                {' '}
+                {photo.user}
+              </span>
+              <span className="faves">
+                <button className="fav-btn" type="button" id="faveButton" key={photo.photoId} onClick={ClickMe}>
+                  <img
+                    className="star"
+                    src="https://img.icons8.com/android/24/ffffff/star.png"
+                    alt="favIcon"
+                  />
+                </button>
+                <span className="fav-count" id="favNum" key={photo.photoId}>
+                  {photo.favs}
+                </span>
+              </span>
+              <span className="comments">
+                <img className="comment-icon" src="https://img.icons8.com/ios/50/ffffff/topic.png" alt="commentIcon" width="25px" height="25px" />
+                {photo.comments}
+              </span>
+            </span>
+
           </div>
-      </div> */
-      ))}
+        ))}
+        <div />
+      </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default Faves;
