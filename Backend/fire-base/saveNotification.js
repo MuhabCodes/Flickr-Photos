b9bad73f773c34f8e4bcd23311b40dd80119c86e
Ahmed Ehab <Ahmed.bahloul00@eng-st.cu.edu.eg>
2021-06-01 21:12:38 +0200

    linter

--- Backend/fire-base/saveNotification.js
+++ Backend/fire-base/saveNotification.js
@@ -8,11 +8,7 @@ module.exports = async function SaveNotification(notification) {
         databaseURL: 'https://notify-d4836-default-rtdb.firebaseio.com',
       });
     }
-    const FIREBASE_AUTH = admin.auth();
-    const FIREBASE_MESSAGING = admin.messaging();
     const FIREBASE_DATABASE = admin.database();
-
-    const ref = FIREBASE_DATABASE.ref('/notifications');
 
     const doc = await FIREBASE_DATABASE.ref('/notifications').push(notification);
     if (doc) { return doc; }
