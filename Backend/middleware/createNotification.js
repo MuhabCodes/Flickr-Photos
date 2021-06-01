75b9d1c798cf0332e53e5c20236657007588a233
Ahmed Ehab <Ahmed.bahloul00@eng-st.cu.edu.eg>
2021-06-01 19:51:46 +0200

    updated sent notification

--- Backend/middleware/createNotification.js
+++ Backend/middleware/createNotification.js
@@ -1,6 +1,8 @@
 const SaveNotification = require('../fire-base/saveNotification');
 const Notification = require('../components/notification/notificationModel');
 const { getPhotoById } = require('../components/photo/photoDAL');
+const {getUserById} = require('../components/user/userDAL');
+
 
 const createLikeNotification = async (req, res) => {
   const { photoId } = req.params;
@@ -18,24 +20,41 @@ const createLikeNotification = async (req, res) => {
         favoriteCreated,
       }); // this mean dont do anything if he doesnt find photo
     }
-  
+    const recieverInfo = await getUserById(reciever);
+    const senderInfo = await getUserById(req.userId);
+    let senderName=senderInfo.displayName;
+    let recieverName= recieverInfo.displayName;
+    console.log("senderName",senderName);
+    console.log("recieverName",recieverName);
+    console.log("req.body.favoriteDate",req.body.favoriteDate);
     const newNotification = new Notification({
       sender:req.userId,
+      senderName,
       reciever, // reciever.toString()
+      recieverName,
       act: `like`,
       photoId,
       notificationDate: req.body.favoriteDate,
+      imageUrl:photo.imageUrl,
     });
     console.log('newNot', newNotification);
     // firebase dont allow ".", "#", "$", "/", "[", or "]" in keys
+
+    // since firebase dont allow undefined values and throw errors
+    senderName = senderName==undefined? null : senderName;
+    recieverName = recieverName==undefined? null : recieverName;
+    
     const id = newNotification._id;
     const firebaseNotification = { // due to firebase constrictions
       sender: newNotification.sender.toString(),
+      senderName,
       reciever: newNotification.reciever.toString(),
+      recieverName,
       id: id.toString(),
       act: newNotification.act,
       photoId: newNotification.photoId.toString(),
-      notificationDate: newNotification.notificationDate.toString(),
+      notificationDate: req.body.favoriteDate.toString(),
+      imageUrl:photo.imageUrl.toString(),
     };
     console.log('firebasenot', firebaseNotification);
     await SaveNotification(firebaseNotification);
@@ -55,23 +74,39 @@ const createCommentNotification = async (req, res) => {
 
   try {
 
-  const commentCreated = req.commentCreated;
-  const photoId =req.photoFound._id; 
+    const commentCreated = req.commentCreated;
+    const photoId =req.photoFound._id; 
 
-  const reciever =req.photoFound.user; // photo_owner
+    const reciever =req.photoFound.user; // photo_owner
+    const recieverInfo = await getUserById(reciever);
+    const senderInfo = await getUserById(req.userId);
+    let senderName=senderInfo.displayName;
+    let recieverName= recieverInfo.displayName;
+
+    console.log(senderName,recieverName);
     const newNotification = new Notification({
       sender:req.userId,
       reciever, // reciever.toString()
       act: `comment`,
       photoId:req.photoFound._id,
       notificationDate: commentCreated.dateCreated,
+      recieverName,
+      imageUrl:req.photoFound.imageUrl,
+      senderName,
     });
     console.log('newNot', newNotification);
     // firebase dont allow ".", "#", "$", "/", "[", or "]" in keys
     const id = newNotification._id;
+    // since firebase dont allow undefined values and throw errors
+    senderName = senderName==undefined? null : senderName;
+    recieverName = recieverName==undefined? null : recieverName;
+
     const firebaseNotification = { // due to firebase constrictions
       sender: newNotification.sender.toString(),
       reciever: newNotification.reciever.toString(),
+      recieverName,
+      senderName,
+      imageUrl:req.photoFound.imageUrl,
       id: id.toString(),
       act: newNotification.act,
       photoId: newNotification.photoId.toString(),
