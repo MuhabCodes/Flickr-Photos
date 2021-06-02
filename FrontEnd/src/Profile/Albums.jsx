87a46a36d93f3c4ab7b52c5a66b025fab8a02b3c
AhmedM20 <55909284+AhmedM20@users.noreply.github.com>
2021-06-02 18:53:46 +0200

    albums page

---
+++ FrontEnd/src/Profile/Albums.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router';
+import { Link, useHistory } from 'react-router-dom';
+import axios from 'axios';
+import jwt from 'jwt-decode';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Albums.css';
+
+function Albums() {
+  const history = useHistory(); // useHitory to redirect the user
+  const userjwt = jwt(localStorage.getItem('token'));
+  // eslint-disable-next-line no-unused-vars
+  const [enabled, setEnabled] = useState(false); // check if user
+  const { routeId } = useParams(); // grab the id of the photo to fetch
+  const [data, setData] = useState(null);
+  const [albumsData, setAlbumsData] = useState(null);
+  useEffect(() => {
+    if (routeId) {
+      axios.get(`/albums/${routeId}`) // fetch data with specific album
+        .then((resp) => {
+          setData(resp.data);
+          setAlbumsData(resp.data.userAlbums);
+          // check if it is the user's album to set enable to true to give rights
+          // eslint-disable-next-line eqeqeq
+          if (resp.data.userId == userjwt.sub) { setEnabled(true); }
+          return resp.data;
+        }).catch(() => { history.push('*'); });
+    }
+  }, []);
+
+  // Render albums for preview
+  const renderPhotos = (source) => source.map((photo) => (
+    <div key={photo.albumCover}>
+
+      <div id="album-container">
+        <img
+          id="added-photos"
+          src={photo.albumCover}
+          alt=""
+        />
+        <Link id="album-title" to="*">{photo.albumTitle}</Link>
+        <span id="photo-number">
+          {photo.albumphotos.length}
+          {' '}
+          photos
+        </span>
+      </div>
+    </div>
+  ));
+
+  return (data ? (
+    <div id="result">{albumsData && renderPhotos(albumsData)}</div>) : <div>loading...</div>
+  );
+}
+
+export default Albums;
