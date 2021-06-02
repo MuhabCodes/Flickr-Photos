06e61199df8b033d8898cba1f1be3fad343755e2
yousefQadry <58371826+yousefQadry@users.noreply.github.com>
2021-06-02 21:52:59 +0200

    Update getBrands.js

--- NewDocs/camera/getBrands.js
+++ NewDocs/camera/getBrands.js
@@ -12,21 +12,77 @@
  * @apiSuccess (Success 200) {Object[]} cameraBrands all brands of cameras that flicker knows
  * @apiSuccess (Success 200) {Number} statusCode The status code
  * @apiSuccessExample {json} Success-Response:
- *    {
-   "brands": [
-      {
-         "id": "canon",
-         "text": "Canon"
-      },
-      {
-         "id": "nikon",
-         "text": "Nikon"
-      },
-      {
-         "id": "apple",
-         "text": "Apple"
-      }
-   ]
+ *   {
+    "cameras": [
+        {
+            "topModels": [
+                "Nikon 500s",
+                "D850",
+                "D500"
+            ],
+            "modelTypes": [
+                "Digital SLR",
+                "Mirrorless Camera",
+                "Point and Shoot"
+            ],
+            "_id": "1092ea18321fa5101115dfad",
+            "name": "Nikon",
+            "image": "https://live.staticflickr.com/cameras/72157647510174616_model_large_d5b7ef6582.jpg",
+            "__v": 0
+        },
+        {
+            "topModels": [
+                "EOS 5D Mark III",
+                "CANON EOS 5D MARK IV",
+                "Canon600D"
+            ],
+            "modelTypes": [
+                "Digital SLR",
+                "Film Scanner",
+                "Mirrorless Camera",
+                "Point and Shoot",
+                "Video Camera"
+            ],
+            "_id": "1092ea18321fa5101115dfae",
+            "name": "Canon",
+            "image": "https://live.staticflickr.com/cameras/72157622292089908_model_large_497d3698ba.jpg",
+            "__v": 0
+        },
+        {
+            "topModels": [
+                "iPhone 7",
+                "iPhone XR",
+                "iPhone X"
+            ],
+            "modelTypes": [
+                "Cameraphone",
+                "Point and Shoot",
+                "Tablet"
+            ],
+            "_id": "1092ea18321fa5101115dfaf",
+            "name": "Apple",
+            "image": "https://live.staticflickr.com/cameras/72157635062339950_model_large_d77764305a.jpg",
+            "__v": 0
+        },
+        {
+            "topModels": [
+                "Galaxy S7",
+                "Galaxy S8",
+                "Galaxy S6"
+            ],
+            "modelTypes": [
+                "Cameraphone",
+                "Digital SLR",
+                "Mirrorless Camera",
+                "Point and Shoot",
+                "Video Camera"
+            ],
+            "_id": "1092ea18321fa5101115dfb0",
+            "name": "Samsung",
+            "image": "https://mob4me.com/Assets/images/mobile/galaxy-s8-large.jpg",
+            "__v": 0
+        }
+    ]
 }
  * 
  *  
