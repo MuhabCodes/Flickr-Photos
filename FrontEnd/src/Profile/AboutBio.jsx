715b7741827bee9c41640e68fa3d016c464c6350
manaarr <manaarrzakaria@gmail.com>
2021-06-02 22:12:00 +0200

    adjusted default

--- FrontEnd/src/Profile/AboutBio.jsx
+++ FrontEnd/src/Profile/AboutBio.jsx
@@ -2,10 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import axios from 'axios';
 import jwt from 'jwt-decode';
+import configData from '../config.json';
 import edit from './assets/edit_icon.png';
 import './AboutBio.css';
 
 const AboutBio = () => {
+  axios.defaults.baseURL = `${configData.SERVER_URL}`;
+  axios.defaults.headers.common['Content-Type'] = 'application/json';
+  axios.defaults.headers.common.Authorization = localStorage.getItem('token');
   const { id } = useParams();
   const history = useHistory();
   const [isLoading, setLoading] = useState(true);
@@ -16,7 +20,13 @@ const AboutBio = () => {
     axios.get(`/Userinfo/${id}`, {
     }).then((resp) => {
       setLoading(false); // set loading to false as it is dont and fetched data
-      setText(resp.data.bio);
+      if (resp.data.bio === '0') {
+        if (currUser) {
+          setText('Write a little about yourself');
+        }
+      } else {
+        setText(resp.data.bio);
+      }
       return resp.data;
     }).catch((error) => {
       if (error.response.status === 401) {
@@ -44,28 +54,46 @@ const AboutBio = () => {
       moreText.style.display = 'inline';
     }
   }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const add = document.getElementById('usertextarea').value;
+    axios.patch(`/Userinfo/${userjwt.sub}`, { bio: add })
+      .then(() => {
+        history.push(`/Profile/About/${id}`);
+      }).catch((error) => {
+        if (error.response.status === 401) {
+          localStorage.removeItem('token'); // remove token and redirect to login if not authorized
+          setTimeout(() => history.push('/login'), 2000); // Redirect to Error page
+        } else if (error.response.status === 404) {
+          setTimeout(() => history.push('*'), 2000); // Redirect to Error page
+        } else {
+          localStorage.removeItem('token'); // remove token and redirect to login if not authorized
+          setTimeout(() => history.push('/login'), 2000); // Redirect to Error page
+        }
+      });
+  };
   return (
-    <div className="about-bio-area">
+    <div className="about-bio-area" id="render-bio-abu">
       {!isLoading && (
       <div className="bio-container">
-        {/* edit bio button for user */}
-        {currUser && (
-        <button
-          type="button"
-          id="user-edit-biobtn"
-          className="edit-button"
-          onClick={() => { // hide text area and show edit text area
-            document.getElementById('textcontainer').style.display = 'none';
-            if (text.length > 200) {
-              document.getElementById('readbtn').style.display = 'none';
-            }
-            document.getElementById('editingarea').style.display = 'block';
-            document.getElementById('usertextarea').innerHTML = text;
-          }}
-        >
-          <img src={edit} alt="" className="edit-button-img" />
-        </button>
-        )}
+          {/* edit bio button for user */}
+          {currUser && (
+          <button
+            type="button"
+            id="user-edit-biobtn"
+            className="edit-button"
+            onClick={() => { // hide text area and show edit text area
+              document.getElementById('textcontainer').style.display = 'none';
+              if (text.length > 200) {
+                document.getElementById('readbtn').style.display = 'none';
+              }
+              document.getElementById('editingarea').style.display = 'block';
+              document.getElementById('usertextarea').innerHTML = text;
+            }}
+          >
+            <img src={edit} alt="" className="edit-button-img" />
+          </button>
+          )}
         <div className="edit-bio" id="editingarea" style={{ display: 'none' }}>
           <textarea name="userbio" id="usertextarea" />
           <div className="editbio-actions">
@@ -73,13 +101,14 @@ const AboutBio = () => {
               type="button"
               className="save-bio"
               id="editbio-savebtn"
-              onClick={() => { // save changes to text value and hide editing area, show text area
+              onClick={(e) => { // save changes to text value and hide editing area, show text area
                 setText(document.getElementById('usertextarea').value);
                 document.getElementById('textcontainer').style.display = 'block';
                 if (text.length > 200) {
                   document.getElementById('readbtn').style.display = 'block';
                 }
                 document.getElementById('editingarea').style.display = 'none';
+                handleSubmit(e);
               }}
             >
               Save
@@ -117,11 +146,10 @@ const AboutBio = () => {
                 <button type="button" onClick={Read} id="readbtn">Read more</button>
               </p>
             )}
-
         </div>
         <div className="divider" />
       </div>
-      ) }
+      )}
     </div>
   );
 };
