ec07a42d137415a9fd16deebcad8a6beb85e80d2
Mustafa Usama <mustafausamaa@gmail.com>
2021-06-02 06:16:32 +0200

    Removed ViewPhotos widget and added Navigation to select button

--- Android/lib/profile/camera_roll.dart
+++ Android/lib/profile/camera_roll.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'view_photos.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/photo_provider.dart';
@@ -10,6 +10,8 @@ class CameraRoll extends StatefulWidget {
 }
 
 class _CameraRollState extends State<CameraRoll> {
+  bool isSelected = false;
+
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -80,31 +82,35 @@ class _CameraRollState extends State<CameraRoll> {
           SizedBox(
             height: 15,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
+          Stack(
+            alignment: AlignmentDirectional.topStart,
             children: [
-              SizedBox(
-                width: size.width * 0.05,
-              ),
               ViewPhotos(),
-              Container(
-                height: 30,
-                width: size.width * 0.23,
+              Positioned(
+                left: size.width * 0.7,
                 child: Container(
-                  decoration: BoxDecoration(shape: BoxShape.rectangle),
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text("Select",
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.black,
-                          fontWeight: FontWeight.w600,
-                        )),
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.grey[50],
-                      side: BorderSide(color: Colors.black, width: 2),
+                  height: 30,
+                  width: size.width * 0.23,
+                  child: Container(
+                    decoration: BoxDecoration(shape: BoxShape.rectangle),
+                    child: OutlinedButton(
+                      onPressed: () {
+                        setState(() {
+                          Navigator.of(context).pushNamed(
+                            '/selectphotos',
+                          );
+                        });
+                      },
+                      child: Text("Select",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600,
+                          )),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.grey[50],
+                        side: BorderSide(color: Colors.black, width: 2.0),
+                      ),
                     ),
                   ),
                 ),
@@ -114,83 +120,5 @@ class _CameraRollState extends State<CameraRoll> {
         ],
       ),
     );
-  }
-}
-
-class ViewPhotos extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var photoProvider = Provider.of<PhotoProvider>(context, listen: true);
-    var size = MediaQuery.of(context).size;
-    return IndexedStack(children: [
-      Wrap(
-          direction: Axis.vertical,
-          spacing: 10,
-          //runSpacing: 20,
-          children: List.generate(
-              photoProvider.dateTaken
-                  ? photoProvider.photosWithUploadDate.length
-                  : photoProvider.photosWithCaptureDate.length, (index) {
-            return Container(
-                //height: size.height * 0.2,
-                width: size.width * 0.7,
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        width: size.width * 0.7,
-                        child: Text(
-                          DateFormat("EEEE — MMM d,yyyy").format(photoProvider
-                                  .dateTaken
-                              ? photoProvider.photosWithUploadDate[index].date
-                              : photoProvider
-                                  .photosWithCaptureDate[index].date),
-                          overflow: TextOverflow.fade,
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.w600),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Wrap(
-                        direction: Axis.horizontal,
-                        children: List.generate(
-                          photoProvider.dateTaken
-                              ? photoProvider
-                                  .photosWithUploadDate[index].images.length
-                              : photoProvider
-                                  .photosWithCaptureDate[index].images.length,
-                          (index2) {
-                            return Container(
-                              child: Wrap(
-                                children: [
-                                  Container(
-                                    margin:
-                                        EdgeInsets.only(right: 5, bottom: 5),
-                                    child: Image.network(
-                                      photoProvider.dateTaken
-                                          ? photoProvider
-                                              .photosWithUploadDate[index]
-                                              .images[index2]
-                                              .imagePath
-                                          : photoProvider
-                                              .photosWithCaptureDate[index]
-                                              .images[index2]
-                                              .imagePath,
-                                      fit: BoxFit.cover,
-                                      height: 100,
-                                      width: 100,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            );
-                          },
-                        ),
-                      )
-                    ]));
-          }))
-    ]);
   }
 }
