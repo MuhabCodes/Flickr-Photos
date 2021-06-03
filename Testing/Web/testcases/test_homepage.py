d61253ecce15405e4a3ae73b63d52c93ce43ba21
George-Joseph99 <george_joseph99@hotmail.com>
2021-06-03 20:31:31 +0200

    Update test_homepage.py

--- Testing/Web/testcases/test_homepage.py
+++ Testing/Web/testcases/test_homepage.py
@@ -52,53 +52,62 @@ class TestHomePage(object):
     def test_check_bottom_links2(self, setup):
         if self.homePage.element_clickable(self.homePage.JOBS_LINK, 5) == 1:
             self.homePage.click(self.homePage.JOBS_LINK)
+            sleep(5)
             assert self.homePage.page_url() == TestData.JOBS_URL
         print("\nJobs link is enabled and redirects to Jobs page")
 
     def test_check_bottom_links3(self, setup):
         if self.homePage.element_clickable(self.homePage.BLOG_LINK, 5) == 1:
             self.homePage.click(self.homePage.BLOG_LINK)
+            sleep(5)
             assert self.homePage.page_url() == TestData.BLOG_URL
         print("\nBlog link is enabled and redirects to Blog page")
 
     def test_check_bottom_links4(self, setup):
         if self.homePage.element_clickable(self.homePage.DEVELOPERS_LINK, 5) == 1:
             self.homePage.click(self.homePage.DEVELOPERS_LINK)
+            sleep(5)
             assert self.homePage.page_url() == TestData.DEVELOPERS_URL
         print("\nDevelopers link is enabled and redirects to Developers page")
 
     def test_check_bottom_links5(self, setup):
         if self.homePage.element_clickable(self.homePage.GUIDELINES_LINK, 5) == 1:
             self.homePage.click(self.homePage.GUIDELINES_LINK)
+            sleep(5)
             assert self.homePage.page_url() == TestData.GUIDELINES_URL
         print("\nGuidelines link is enabled and redirects to Guidelines page")
 
     def test_check_bottom_links6(self, setup):
         if self.homePage.element_clickable(self.homePage.HELP_LINK, 5) == 1:
             self.homePage.click(self.homePage.HELP_LINK)
+            sleep(5)
             assert self.homePage.page_url() == TestData.HELP_URL
         print("\nHelp link is enabled and redirects to Help page")
 
     def test_check_bottom_links7(self, setup):
         if self.homePage.element_clickable(self.homePage.HELP_FORUM_LINK, 5) == 1:
             self.homePage.click(self.homePage.HELP_FORUM_LINK)
+            sleep(5)
             assert self.homePage.page_url() == TestData.HELP_FORUM_URL
         print("\nHelp forum link is enabled and redirects to Help forum page")
 
     def test_check_bottom_links8(self, setup):
         if self.homePage.element_clickable(self.homePage.PRIVACY_LINK, 5) == 1:
             self.homePage.click(self.homePage.PRIVACY_LINK)
+            sleep(5)
             assert self.homePage.page_url() == TestData.PRIVACY_URL
         print("\nPrivacy link is enabled and redirects to Privacy page")
 
     def test_check_bottom_links9(self, setup):
         if self.homePage.element_clickable(self.homePage.TERMS_LINK, 5) == 1:
             self.homePage.click(self.homePage.TERMS_LINK)
+            sleep(5)
             assert self.homePage.page_url() == TestData.TERMS_URL
         print("\nTerms link is enabled and redirects to Terms page")
 
     def test_check_bottom_links10(self, setup):
         if self.homePage.element_clickable(self.homePage.COOKIES_LINK, 5) == 1:
             self.homePage.click(self.homePage.COOKIES_LINK)
+            sleep(5)
             assert self.homePage.page_url() == TestData.COOKIES_URL
         print("\nCookies link is enabled and redirects to Cookies page")
