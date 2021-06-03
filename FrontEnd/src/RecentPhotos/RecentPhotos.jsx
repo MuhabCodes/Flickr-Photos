ff09d5fb4b86dbfa2aeeedd69a7b110d58494763
YomnaTarek <yomnaragab13@gmail.com>
2021-06-03 17:15:36 +0200

    Added comments in RecentPhotos.jsx

--- FrontEnd/src/RecentPhotos/RecentPhotos.jsx
+++ FrontEnd/src/RecentPhotos/RecentPhotos.jsx
@@ -3,30 +3,20 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 import axios from 'axios';
 import $ from 'jquery';
 import jwt from 'jwt-decode';
-// import configData from '../config.json';
 // The RecentPhotos.jsx is the component which helps in displaying the recent photos,
-// It includes the function 'RecentPhotos' which has a code the helps in fetching the photos from
-// a mock server. This function returns the fetched images, but not all at once,
+// It includes the function 'RecentPhotos' which has a code the helps in fetching the photos.
+// This function returns the fetched images, but not all at once,
 // but a couple at a time and the more the user scrolls down,
 // the more images are fetched and displayed.
+// Furthermore, if the user logged in, he can see each photo's details
+// (username, image's title, fav button, fav count and comment count)
+// Meanwhile, if the user is a guest (not logged in), he cannot see these details.
+
 const RecentPhotos = () => {
-  // const [photos, setRecPhotos] = useState([]);
-  // const [toggled, setToggle] = useState(false);
-  // const ph = props;
-  // const { photos, loading } = ph;
-  // useEffect helps us fetch the photos from the mock server.
-  // useEffect(() => {
-  //   fetch(`${configData.SERVER_URL}/photosExplore`)
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setRecPhotos(data);
-  //     })
-  //     .catch(() => {
-  //     });
-  // }, []);
-  const [photos, setRecPhotos] = useState([]);
-  const [loading, setLoading] = useState(false);
-  // useEffect helps us fetch the photos from the mock server.
+  const [photos, setRecPhotos] = useState([]); // sets the images fetched
+  const [loading, setLoading] = useState(false); // for loading purpose
+  // The following loadPage function, checks if the user is a guest or logged in and displays
+  // images details accordingly.
   let userjwt = [];
   function loadPage() {
     if (localStorage.getItem('token')) {
@@ -34,40 +24,22 @@ const RecentPhotos = () => {
       $('.text-area').css('display', 'block');
     }
   }
-  // useEffect helps us fetch the photos from the mock server.
+  // useEffect helps us fetch the photos from the server.
   useEffect(() => {
     axios.get('/photosExplore?_sort=dateUploaded&_order=desc')
       .then((resp) => {
         setLoading(false);
         setRecPhotos(resp.data);
-        // setFaveCounts(resp.data.favs);
       });
   }, []);
-
-  // const obj = JSON.parse(photos);
-  // console.log(obj.favs);
   // The following changes the photos(objects) to array that can be used by the .map
   // function in the return block.
   const photoArr = Array.from(photos);
-  // console.log(photoArr[1].favs);
-  // function IncFave() {
-  //   if (toggled === true) {
-  //     const count = document.getElementById('favNum').innerText;
-  //     console.log(count);
-  //   }
-  // }
   // The following function toggles the fave icon on click
   function ClickMe(e) {
     if (userjwt) {
       if (e.target.getAttribute('src') === 'https://img.icons8.com/android/24/ffffff/star.png') {
         e.target.setAttribute('src', 'https://img.icons8.com/ios-filled/25/ffffff/star--v1.png');
-        // let FavesC = { faveCounts };
-        // FavesC += 1;
-        // axios.patch(`/photos?title=${search}`, FavesC)
-        //   .then(() => {
-        //     setFaveCounts(FavesC);
-        //     history.push(`/search?title=${search}`);
-        //   });
       } else if (e.target.getAttribute('src') === 'https://img.icons8.com/ios-filled/25/ffffff/star--v1.png') {
         e.target.setAttribute('src', 'https://img.icons8.com/android/24/ffffff/star.png');
       }
@@ -78,10 +50,10 @@ const RecentPhotos = () => {
   if (loading) {
     return <h1>Loading</h1>;
   }
-  // The following part is the one returns the fetched images.
+  // The following part returns the fetched images.
   // The lazy load Image component is used to display the images but not all at once,
   // when the user scrolls down more images are fetched to be displayed.
-  // The next lines are to display the image description, username, faves and comments count
+  // The next lines are to display the image title, username, faves and comments count
   // when hovering on an image
   return (
     <div className="recent-photos">
