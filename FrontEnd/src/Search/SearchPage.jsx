5ea1f301dabb42cace28f051868b2e4194944ce2
YomnaTarek <yomnaragab13@gmail.com>
2021-06-03 19:30:30 +0200

    Adjusted classNames to follow naming convention

--- FrontEnd/src/Search/SearchPage.jsx
+++ FrontEnd/src/Search/SearchPage.jsx
@@ -4,11 +4,11 @@ import NavBar from '../App/Navbar';
 // This component is the main component for the search functionality where we gather here
 // the main flickr navbar along with the MainSearch component to be displayed on the same page.
 const SearchPage = () => (
-  <div className="Search-Page">
-    <div className="Nav-Bar">
+  <div className="search-page">
+    <div className="nav-bar">
       <NavBar />
     </div>
-    <div className="search content">
+    <div className="search-content">
       <MainSearch />
     </div>
   </div>
