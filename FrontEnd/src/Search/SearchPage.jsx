45e92bc84e03fa94a1ebd6025fccadf04a62452a
YomnaTarek <yomnaragab13@gmail.com>
2021-06-03 17:16:44 +0200

    Added comments in SearchPage.jsx

--- FrontEnd/src/Search/SearchPage.jsx
+++ FrontEnd/src/Search/SearchPage.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import MainSearch from './MainSearch';
 import NavBar from '../App/Navbar';
-
+// This component is the main component for the search functionality where we gather here
+// the main flickr navbar along with the MainSearch component to be displayed on the same page.
 const SearchPage = () => (
   <div className="Search-Page">
     <div className="Nav-Bar">
