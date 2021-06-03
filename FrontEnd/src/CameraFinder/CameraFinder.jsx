1a28831bfd4a8c63154fb0a38755612c84dbff24
YomnaTarek <yomnaragab13@gmail.com>
2021-06-03 17:12:22 +0200

    Updated comments in CameraFinder.jsx

--- FrontEnd/src/CameraFinder/CameraFinder.jsx
+++ FrontEnd/src/CameraFinder/CameraFinder.jsx
@@ -8,16 +8,18 @@ import NavBar from '../App/Navbar';
 // This .jsx will include the components that will make up the Camera Finder webpage
 // The following function includes:
 // 1- useState that will help us set our data fetched
-// 2- useEffect function that will fetch the data from our json file
+// 2- useEffect function that will fetch the data from our server
 // 3- returns the title and subtitles of the webpage
 // 4- returns the components that will make up the page:
 // * The Most Popular Brands section which includes images, brand names and brand models
 // * Graphs have been added
-// * The Rank Table which includes all the brands in the .json and displays details such as
-// Rank, Brand, Top Models, Model Types, and # of Models.
+// * The Rank Table which includes all the brands and displays details such as
+// Brand, Top Models, Model Types.
+// This page is available to all kinds of users (logged or guests).
 
 const CameraFinder = () => {
-  const [cameras, setPopCam] = useState([]);
+  const [cameras, setPopCam] = useState([]); // sets the cameras fetched from the server
+  // useEffect and axios used to make get request to fetch cameras' details
   useEffect(() => {
     axios.get('/cameras')
       .then((resp) => {
