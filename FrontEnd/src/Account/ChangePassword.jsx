b24aaafd6fa775d6136802a932f3f415ec52954f
AhmedMAyoub <ahmed2000mostafa@gmail.com>
2021-06-04 16:43:55 +0200

    Added Error handling to change password

--- FrontEnd/src/Account/ChangePassword.jsx
+++ FrontEnd/src/Account/ChangePassword.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-nested-ternary */
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useState } from 'react';
 import axios from 'axios';
@@ -13,6 +15,7 @@ const ChangePassword = () => {
   const history = useHistory();
   // the use of the use state and set state functions
   // to save the changes made in each of this inputs
+  const [changeStatus, setChangeStatus] = useState('');
   const [oldPassword, setOldPassword] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const handleSubmit = (e) => { // Submission of form to put request
@@ -22,11 +25,18 @@ const ChangePassword = () => {
     };
     axios.put('/change-password', ProfileInfo)
       .then(() => {
-        history.push('/account'); // if successful we redirect to account
+        setChangeStatus('Password Changed Successfully, Redirecting to account...');
+        setTimeout(() => history.push('/account'), 2000); // Redirect to Error page
       }).catch((err) => {
         if (err.response.status === 401) {
           localStorage.removeItem('token');
           history.push('/login'); // If unauth error then redirect to login and clear token
+        } else if (err.response.status === 422) {
+          setChangeStatus(0);
+          setChangeStatus('Current Password is not correct');
+        } else if (err.response.status === 404) {
+          setChangeStatus('User with this password doesnt exist');
+          setTimeout(() => history.push('/login'), 2000); // Redirect to Error page
         }
       });
   };
@@ -63,6 +73,9 @@ const ChangePassword = () => {
           </label>
           <button className="change-pw-btn" type="submit" id="change-password-btn">Change Password</button>
         </form>
+        <div className="change-pw-status-div">
+          { changeStatus }
+        </div>
       </div>
     </div>
   );
--- FrontEnd/src/ErrorPages/PasswordMismatch.jsx
+++
@@ -1,5 +0,0 @@
-const PasswordMismatch = () => {
-    return (  );
-}
- 
-export default PasswordMismatch;
