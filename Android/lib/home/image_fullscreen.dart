51013129cb81c7bdb0fc21e6abe2e3d7541dd198
Mustafa Usama <mustafausamaa@gmail.com>
2021-06-05 03:53:40 +0200

    Modified image fullscreen

--- Android/lib/home/image_fullscreen.dart
+++ Android/lib/home/image_fullscreen.dart
@@ -1,13 +1,11 @@
-///[ImageFullscreen] class which displays a given image with zoom in and out feature and
+///[ImageFullscreen] class which displays a given image with zoom in and ou feature and
 ///like comment feature
-
 import 'package:flickr/models/global.dart';
 import 'package:flickr/models/photos.dart';
 import 'package:flickr/models/post.dart';
 import 'package:flickr/profile/profile.dart';
 import 'package:flutter/material.dart';
-import 'package:responsive_widgets/responsive_widgets.dart';
-import 'package:photo_view/photo_view.dart';
+import 'package:share_plus/share_plus.dart';
 
 import 'comments_fav_page.dart';
 import 'comments_page.dart';
@@ -25,7 +23,7 @@ class ImageFullscreen extends StatefulWidget {
 
 class ImageFullscreenState extends State<ImageFullscreen> {
   String textTitle = "";
-  //NetworkImage imagePath = new NetworkImage('www.google.com');
+  //NetworkImage imageUrl = new NetworkImage('www.google.com');
   Photo myPhoto;
   double _widthScreen = 0;
   double _heightScreen = 0;
@@ -40,234 +38,162 @@ class ImageFullscreenState extends State<ImageFullscreen> {
 
   @override
   Widget build(BuildContext context) {
+    bool isClicked = true;
     _widthScreen = MediaQuery.of(context).size.width;
     _heightScreen = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Stack(
-        children: [
-          Center(
-            widthFactor: _widthScreen,
-            child: PhotoView(
-              minScale: PhotoViewComputedScale
-                  .contained, //After zooming out and releasing your finger it returns photo to it's actual size
-              imageProvider: NetworkImage(myPhoto.imagePath),
-            ),
-          ),
-          Align(
-
-              ///to align widgets above each other
-              alignment: Alignment.topLeft,
-              child: Container(
-                //width: _widthScreen,
-                constraints: BoxConstraints(
-                  maxHeight: _heightScreen, /* maxWidth: _widthScreen * 0.2*/
-                ),
-
-                child: Column(
+        appBar: isClicked
+            ? AppBar(
+                backgroundColor: Colors.black,
+                automaticallyImplyLeading: false,
+                title: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  // crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
-                    Column(
+                    Row(
                       children: [
-                        Row(
-                          children: [
-                            Row(
-                              children: [
-                                Column(
-                                  children: [
-                                    Container(
-                                      //this container is related to the circle avatar only (tiny box contains an avatar)
-                                      //color: Colors.white,
-                                      margin: EdgeInsets.only(
-                                          right: 10,
-                                          top: 65,
-                                          left:
-                                              30), //user name padding away from pp
-                                      child: CircleAvatar(
-                                        backgroundImage:
-                                            post.user.profilePicture,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Column(
-                                  children: [
-                                    Container(
-                                      constraints: BoxConstraints(
-                                          maxWidth: _widthScreen * 0.26),
-                                      margin:
-                                          EdgeInsets.only(top: 70, right: 50),
-                                      child: TextButton(
-                                        child: Text(
-                                          post.user.username,
-                                          style: appBarTitleStyle,
-                                          overflow: TextOverflow.ellipsis,
-                                        ),
-                                        onPressed: () {
-                                          /*Navigator.of(context).push(
-                                              MaterialPageRoute(builder: (_) {
-                                            return Profile();
-                                          }));
-                                        */
-                                        },
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                            SizedBox(width: 50),
-                            Column(
-                              children: [
-                                Container(
-                                  margin: EdgeInsets.only(
-                                      right: 5, top: 60, left: 5),
-                                  child: IconButton(
-                                      //padding: EdgeInsets.only(right: 20, top: 50, left: 10),
-                                      icon: Icon(Icons.close_rounded,
-                                          size: 30, color: Colors.white),
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      }),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        ContainerResponsive(
-                          //My Title container
-                          widthResponsive: true,
-                          heightResponsive: true,
-                          margin: EdgeInsets.only(left: 20, bottom: 10),
-                          child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  (post.title != null) ? post.title : "MyTitle",
-                                  style: postTitleStyle,
-                                ),
-                              ]),
+                        Container(
+                          //this container is related to the circle avatar only (tiny box contains an avatar)
+                          //user name padding away from pp
+
+                          child: CircleAvatar(
+                            backgroundImage: NetworkImage(post.user.userAvatar),
+                          ),
                         ),
                         Container(
-                          //Draw horizontal line
-                          margin: EdgeInsets.only(bottom: 5),
-                          constraints: BoxConstraints.expand(
-                              height: 20, width: _widthScreen),
-                          decoration: BoxDecoration(
-                            // 'decoration:' doesn't allow writing 'color:' after or before it
-                            border: Border.all(
-                              color:
-                                  Colors.transparent, // white as border color
-                              width: 0,
+                          child: TextButton(
+                            child: Text(
+                              post.user.username,
+                              style: appBarTitleStyle,
                             ),
-                            color: Colors.transparent,
+                            onPressed: () {
+                              Navigator.of(context)
+                                  .push(MaterialPageRoute(builder: (_) {
+                                return Profile();
+                              }));
+                            },
                           ),
-                          //color: Colors.white,
-                          child: Row(
-                            children: <Widget>[
-                              Padding(
-                                padding: EdgeInsets.symmetric(horizontal: 10.0),
-                                child: Container(
-                                  height: 2.2,
-                                  width: _widthScreen - 21, //340.0,
-                                  color: Colors.grey,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Row(
-                          children: [
-                            Stack(
-                              alignment: Alignment(0, 0),
-                              children: <Widget>[
-                                Icon(
-                                  Icons.star_border,
-                                  size: 35,
-                                  color: post.isLiked
-                                      ? Colors.transparent
-                                      : Colors.grey,
-                                ),
-                                IconButton(
-                                  icon: Icon(Icons.star),
-                                  color: post.isLiked
-                                      ? Colors.white
-                                      : Colors.transparent,
-                                  iconSize: 35,
-                                  onPressed: () {
-                                    addLikers(post);
-
-                                    setState(() {});
-                                  },
-                                ),
-                              ],
-                            ),
-                            IconButton(
-                                padding: EdgeInsets.only(top: 8, left: 15),
-                                icon: Icon(Icons.mode_comment_outlined),
-                                color: Colors.grey,
-                                iconSize: 30,
-                                onPressed: () {
-                                  Navigator.of(context)
-                                      .push(MaterialPageRoute(builder: (_) {
-                                    return CommentsFavs(post, 1, false, 2);
-                                  }));
-                                  setState(() {});
-                                }),
-                            IconButton(
-                                padding: EdgeInsets.only(top: 8, left: 25),
-                                icon: Icon(Icons.share_outlined),
-                                color: Colors.grey,
-                                iconSize: 30,
-                                onPressed: () {
-                                  setState(() {});
-                                }),
-                            IconButton(
-                                padding: EdgeInsets.only(
-                                    top: 9, left: 25, right: 20),
-                                icon: Icon(Icons.info_outline),
-                                color: Colors.grey,
-                                iconSize: 32,
-                                onPressed: () {
-                                  setState(() {});
-                                }),
-                            Column(
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: [
-                                  Text(
-                                    post.likes == null
-                                        ? "0 Faves"
-                                        : (post.likes.length.toString() +
-                                            " Faves"),
-                                    style: appBarTitleStyle,
-                                    textAlign: TextAlign.right,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                  Text(
-                                    post.comments == null
-                                        ? "0 Comments"
-                                        : (post.comments.length.toString() +
-                                            " Comments"),
-                                    style: appBarTitleStyle,
-                                    textAlign: TextAlign.right,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                ]),
-                          ],
                         ),
                       ],
                     ),
+                    Container(
+                      //margin: EdgeInsets.only(right: 5, top: 60, left: 5),
+                      child: IconButton(
+                          //padding: EdgeInsets.only(right: 20, top: 50, left: 10),
+                          icon: Icon(Icons.close_rounded,
+                              size: 30, color: Colors.white),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          }),
+                    )
                   ],
                 ),
-              )),
-        ],
-      ),
-    );
+              )
+            : null,
+        body: Container(
+          decoration: BoxDecoration(color: Colors.black),
+          child: InkWell(
+              onTap: () {
+                setState(() {
+                  isClicked = !isClicked;
+                });
+              },
+              child: Align(
+                  alignment: Alignment.center,
+                  child: Image.network(myPhoto.imageUrl))),
+        ),
+        bottomSheet: Container(
+            height: _heightScreen * 0.15,
+            color: Colors.black,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(
+                    left: 20,
+                    bottom: 10,
+                  ),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      (post.title != null) ? post.title : "MyTitle",
+                      style: postTitleStyle,
+                    ),
+                  ),
+                ),
+                Divider(
+                  height: 10,
+                  color: Colors.white,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(
+                          icon: post.isLiked
+                              ? Icon(Icons.star)
+                              : Icon(Icons.star_border),
+                          color: Colors.grey,
+                          iconSize: 35,
+                          onPressed: () {
+                            addLikers(post);
+
+                            setState(() {});
+                          },
+                        ),
+                        IconButton(
+                            padding: EdgeInsets.only(top: 8, left: 15),
+                            icon: Icon(Icons.mode_comment_outlined),
+                            color: Colors.grey,
+                            iconSize: 30,
+                            onPressed: () {
+                              Navigator.of(context)
+                                  .push(MaterialPageRoute(builder: (_) {
+                                return CommentsFavs(post, 1, false, 2);
+                              }));
+                              setState(() {});
+                            }),
+                        IconButton(
+                            padding: EdgeInsets.only(top: 8, left: 25),
+                            icon: Icon(Icons.share_outlined),
+                            color: Colors.grey,
+                            iconSize: 30,
+                            onPressed: () {
+                              setState(() {
+                                Share.share(myPhoto.imageUrl);
+                              });
+                            }),
+                        IconButton(
+                            padding:
+                                EdgeInsets.only(top: 9, left: 25, right: 20),
+                            icon: Icon(Icons.info_outline),
+                            color: Colors.grey,
+                            iconSize: 32,
+                            onPressed: () {
+                              setState(() {});
+                            }),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          post.likes.length.toString() + (" faves"),
+                          style: TextStyle(color: Colors.white, fontSize: 14),
+                          textAlign: TextAlign.end,
+                        ),
+                        Text(
+                          post.comments.length.toString() + (" comments"),
+                          style: TextStyle(color: Colors.white, fontSize: 14),
+                          textAlign: TextAlign.end,
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            )));
   }
 } //end class
