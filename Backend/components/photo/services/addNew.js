d6e57b07d1006b641b883bc48b7ed81581b685e6
IShamss <mohamedshams2000@gmail.com>
2021-06-05 12:58:22 +0200

    add the addtag to the addphoto

--- Backend/components/photo/services/addNew.js
+++ Backend/components/photo/services/addNew.js
@@ -5,6 +5,11 @@ const addNew = async (photoInfo, photoPath, res) => {
   try {
     // change the location attribute in the photoInfo
     photoInfo.imageUrl = photoPath;
+    // const newTags = [];
+    // for (tag in photoInfo.tags) {
+    //   newTags.push(addTag(tag, photoInfo.user));
+    // }
+    // photoInfo.tags = newTags;
     const newPhoto = new Photo(photoInfo);
     await addNewPhoto(newPhoto);
     return res.json({ statusCode: 201 });
