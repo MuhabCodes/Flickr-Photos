5a4b4c4546d6e852b7bf344916f9d6a04ea34b3a
George-Joseph99 <george_joseph99@hotmail.com>
2021-06-05 03:20:43 +0200

    Create test_userprofilepage.py

---
+++ Testing/Web/testcases/test_userprofilepage.py
@@ -0,0 +1,92 @@
+import pytest
+from pages.userprofilepage.userprofilepage import UserProfilePage
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from time import sleep
+from pages.loginpage.loginpage import LoginPage
+from info.info import TestData
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+class TestUserProfilePage(object):
+    @pytest.fixture()
+    def setup(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.userprofilePage = UserProfilePage(self.driver)
+        self.mockLogin = LoginPage(self.driver)
+        self.mockLogin.log_in("george_joseph99@hotmail.com", "ejnHG3v35gueXhE")
+        self.userprofilePage.driver.maximize_window()
+        self.userprofilePage.click(self.userprofilePage.YOU_LINK)
+        yield
+        self.userprofilePage.driver.quit()
+
+    def test_about_label(self, setup):
+        self.userprofilePage.click(self.userprofilePage.ABOUT_LABEL)
+        sleep(2)
+        assert "people" in self.userprofilePage.page_url()
+        sleep(2)
+
+    def test_photostream_label(self, setup):
+        action = webdriver.ActionChains(self.driver)
+        action.move_by_offset(10, 0)
+        action.perform()
+        self.userprofilePage.click(self.userprofilePage.PHOTOSTREAM_LABEL)
+        sleep(2)
+        assert "photos" in self.userprofilePage.page_url()
+        sleep(2)
+
+    def test_albums_label(self, setup):
+        action = webdriver.ActionChains(self.driver)
+        action.move_by_offset(10, 0)
+        action.perform()
+        self.userprofilePage.click(self.userprofilePage.ALBUMS_LABEL)
+        sleep(2)
+        assert "albums" in self.userprofilePage.page_url()
+        sleep(2)
+
+    def test_faves_label(self, setup):
+        self.userprofilePage.click(self.userprofilePage.FAVES_LABEL)
+        sleep(2)
+        assert "favorites" in self.userprofilePage.page_url()
+        sleep(2)
+
+    def test_galleries_label(self, setup):
+        self.userprofilePage.click(self.userprofilePage.GALLERIES_LABEL)
+        sleep(2)
+        assert "galleries" in self.userprofilePage.page_url()
+        sleep(2)
+
+    def test_groups_label(self, setup):
+        self.userprofilePage.click(self.userprofilePage.GROUPS_LABEL)
+        sleep(2)
+        assert "groups" in self.userprofilePage.page_url()
+        sleep(2)
+
+    def test_stats_label(self, setup):
+        self.userprofilePage.click(self.userprofilePage.STATS_LABEL)
+        sleep(2)
+        assert "stats" in self.userprofilePage.page_url()
+        sleep(2)
+
+    def test_cameraroll_label(self, setup):
+        self.userprofilePage.click(self.userprofilePage.CAMERAROLL_LABEL)
+        sleep(2)
+        assert "cameraroll" in self.userprofilePage.page_url()
+        sleep(2)
+
+    def test_followers_link(self, setup):
+        action = webdriver.ActionChains(self.driver)
+        action.move_by_offset(10, 0)
+        action.perform()
+        self.userprofilePage.click(self.userprofilePage.FOLLOWERS_LINK)
+        sleep(2)
+        assert "rev" and "contacts" in self.userprofilePage.page_url()
+        sleep(2)
+
+    def test_following_link(self, setup):
+        action = webdriver.ActionChains(self.driver)
+        action.move_by_offset(10, 0)
+        action.perform()
+        self.userprofilePage.click(self.userprofilePage.FOLLOWING_LINK)
+        sleep(2)
+        assert "contacts" in self.userprofilePage.page_url() and "rev" not in self.userprofilePage.page_url()
