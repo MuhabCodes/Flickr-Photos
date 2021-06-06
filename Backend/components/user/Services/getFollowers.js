905c0be3dcf6329d12d9253d6a918d65928af4af
Abdelrhman-Hosny <ahosny2792@gmail.com>
2021-06-06 09:42:07 +0200

    finished getFollowers request

--- Backend/components/user/Services/getFollowers.js
+++ Backend/components/user/Services/getFollowers.js
@@ -1,7 +1,17 @@
 const { getFollowersDAL } = require('../userDAL');
 
+function mapFn(user) {
+  return {
+    userAvatar: user.userAvatar,
+    userName: user.displayName,
+    userId: user._id,
+  };
+}
+
 exports.getFollowersServ = async function getFoll(userId) {
   const followers = await getFollowersDAL(userId);
 
-  return followers;
+  const followersMapped = followers.followers.map(mapFn);
+
+  return { followers: followersMapped };
 };
