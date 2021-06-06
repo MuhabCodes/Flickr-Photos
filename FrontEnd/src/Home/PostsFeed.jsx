f20d947f44872cabe0ff787db1a9868c08d134aa
AhmedMAyoub <ahmed2000mostafa@gmail.com>
2021-06-06 21:29:04 +0200

    Added Fetching of posts

--- FrontEnd/src/Home/PostsFeed.jsx
+++ FrontEnd/src/Home/PostsFeed.jsx
@@ -3,33 +3,37 @@ import { React, useState, useEffect } from 'react';
 import axios from 'axios';
 import jwt from 'jwt-decode';
 import { useHistory } from 'react-router-dom';
+import Post from './Post';
 import configData from '../config.json';
 
 const PostsFeed = () => {
+  const [posts, setPosts] = useState('');
   axios.defaults.baseURL = `${configData.SERVER_URL}`;
   axios.defaults.headers.common['Content-Type'] = 'application/json';
   axios.defaults.headers.common.Authorization = localStorage.getItem('token'); // Applying global default settings from axios
   const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const history = useHistory();
   const userjwt = jwt(localStorage.getItem('token'));
   useEffect(() => {
-    axios.get(`/users/${userjwt.sub}`, {
+    axios.get('/posts', {
     }).then((resp) => {
       setLoading(false);
+      setPosts(resp.data);
       return resp.data;
-    }).catch((error) => {
-      if (error.response.status === 401) {
-        localStorage.removeItem('token'); // remove token and redirect to login if not authorized
-        history.push('/login');
-      } else {
-        localStorage.removeItem('token'); // remove token and redirect to login if not authorized
-        setTimeout(() => history.push('/login'), 100); // Redirect to Error page
-      }
+    }).catch((err) => {
+      setError(err.response.data.message);
     });
   }, []);
   return (
     <div className="posts-main-container">
-      Main Page Feed
+      { isLoading && <div className="">Loading....</div> }
+      { error && <div>{ error }</div> }
+      { posts && posts.map((post) => (
+        <div className="single-post" key={post.photoId}>
+          <Post post={post} />
+        </div>
+      )) }
     </div>
   );
 };
