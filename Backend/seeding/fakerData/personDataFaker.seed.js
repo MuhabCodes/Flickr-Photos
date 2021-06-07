38b277e8d5572de1f5fcfa8e4fb76e2f82c6d210
Abdelrhman-Hosny <ahosny2792@gmail.com>
2021-06-07 09:17:20 +0200

    rename person data generator file

---
+++ Backend/seeding/fakerData/personDataFaker.seed.js
@@ -0,0 +1,33 @@
+const faker = require('faker');
+const Person = require('../../components/person/personModel');
+
+function generatePersonData(userArray) {
+  const personArray = [];
+
+  userArray.forEach((element) => {
+    const age = Math.floor(18 + Math.random() * 40);
+    const realName = `${faker.name.firstName()} ${faker.name.lastName()}`;
+    const dateCreated = faker.date.between('01-01-2015', '06-07-2021');
+    const description = faker.lorem.sentence();
+    const homeTown = faker.address.cityName();
+    const city = faker.address.cityName();
+    const country = faker.address.country();
+    const occupation = faker.name.jobTitle();
+    const personObj = new Person({
+      age,
+      realName,
+      dateCreated,
+      description,
+      homeTown,
+      city,
+      country,
+      occupation,
+    });
+    element.personId = personObj._id;
+    personArray.push(personObj);
+  });
+
+  return personArray;
+}
+
+module.exports.generatePersonData = generatePersonData;
