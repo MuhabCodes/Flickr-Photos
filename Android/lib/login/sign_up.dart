14e6ae1d7f7f804e5b8bebe56918e27fa7d9c604
mehrez1110 <mohamed.ali00@eng-st.cu.edu.eg>
2021-06-07 07:15:02 +0200

    update sign up

--- Android/lib/login/sign_up.dart
+++ Android/lib/login/sign_up.dart
@@ -9,6 +9,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:responsive_widgets/responsive_widgets.dart';
 import 'package:url_launcher/url_launcher.dart';
+
 import '../models/user.dart';
 import '../providers/auth.dart';
 
