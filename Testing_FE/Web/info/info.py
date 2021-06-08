7b87f211a41f19cd721d592cd760f29686de53c0
George-Joseph99 <george_joseph99@hotmail.com>
2021-06-08 21:42:07 +0200

    Create info.py

---
+++ Testing_FE/Web/info/info.py
@@ -0,0 +1,19 @@
+# Contains URLs needed for testing
+
+class TestData(object):
+    MAIN_URL = "http://localhost:3000/"
+    LOGIN_URL = "http://localhost:3000/login"
+    FORGOT_PASSWORD_URL = "http://localhost:3000/forgotpassword"
+    CHECK_EMAIL_FORGOT_PASS_URL = "http://localhost:3000/sendemail"
+    SIGNUP_URL = "http://localhost:3000/signup"
+    ACCOUNT_URL = "http://localhost:3000/account"
+    CHECK_EMAIL_URL = "https://identity.flickr.com/check-email/sign-up"
+    PASSWORD_CHANGE_URL = "https://identity.flickr.com/change-password"
+    PASSWORD_CHANGE_SUCCESS_URL = "https://identity.flickr.com/change-complete/change-password"
+    ACCOUNT_DELETE_URL = "https://www.flickr.com/account/delete"
+    PROFILE_EDIT_URL = "https://www.flickr.com/profile_edit.gne?from=personal"
+    DISPLAY_NAME_URL = "https://www.flickr.com/account/prefs/screenname/?from=personal"
+    FLICKR_MAIL_URL = "https://www.flickr.com/mail"
+    RECENT_ACTIVITY_URL = "https://www.flickr.com/activity"
+    PRIVACY_PERMISSIONS_URL = "https://www.flickr.com/account/privacy"
+    SHARING_EXTENDING_URL = "https://www.flickr.com/account/sharing"
