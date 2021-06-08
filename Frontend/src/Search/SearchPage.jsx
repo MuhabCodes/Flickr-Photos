799d02c104b38105b6c971441ce36c4f40b035ee
YomnaTarek <yomnaragab13@gmail.com>
2021-06-08 21:40:41 +0200

    Added functional documentation to SearchPage.jsx

--- Frontend/src/Search/SearchPage.jsx
+++ Frontend/src/Search/SearchPage.jsx
@@ -3,6 +3,13 @@ import MainSearch from './MainSearch';
 import NavBar from '../App/Navbar';
 // This component is the main component for the search functionality where we gather here
 // the main flickr navbar along with the MainSearch component to be displayed on the same page.
+/**
+ * This is the main component of the search page where flickr's nav bar component and main search
+ * component are rendered.
+ * @component
+ * @function SearchPage
+ * @returns {null} -shows the components of the search page.
+ */
 const SearchPage = () => (
   <div className="search-page">
     <div className="nav-bar">
