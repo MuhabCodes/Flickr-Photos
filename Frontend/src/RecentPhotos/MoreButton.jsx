f7cf2549142a35bc36f0e29a7f0f8912be39d818
YomnaTarek <yomnaragab13@gmail.com>
2021-06-08 21:34:55 +0200

    Added functional documentation for MoreButton.jsx

--- Frontend/src/RecentPhotos/MoreButton.jsx
+++ Frontend/src/RecentPhotos/MoreButton.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
-
+/**
+ * This component renders the More button in the Explore nav bar.
+ * @component
+ * @function MoreButton
+ * @returns {null} -returns the more button as a dropdown button that displays a menu.
+ */
 const MoreButton = () => (
   <button className="dropdown-toggle" type="button" data-testid="more-test" id="exp-dropdown-menu-button1" data-toggle="dropdown" aria-expanded="false">
     More
